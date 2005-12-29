9f72187b399ded6d0e597800f3a0c879e9aa18af
Michael Neale <michael.neale@gmail.com>
2005-12-29 08:07:31 +0000

    got basics working
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@1957 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-repository/src/test/java/org/drools/repository/db/RepositoryImplTest.java
+++ drools-repository/src/test/java/org/drools/repository/db/RepositoryImplTest.java
@@ -1,21 +1,40 @@
 package org.drools.repository.db;
 
+import java.util.List;
+
 import org.drools.repository.RuleDef;
 
 import junit.framework.TestCase;
 
-public class RepositoryImplTest extends HibernateTester {
+public class RepositoryImplTest extends TestCase {
 
     public void testStoreNewRuleDef() throws Exception {
-       
-        RepositoryImpl repo = new RepositoryImpl();
+        RepositoryImpl repo = getRepo();
         RuleDef def = repo.addNewRule("myRule", "A rule", "some comment");
-        assertNotNull(def.getId());
+        assertNotNull(def.getId());        
+        System.out.println(def.getId());
         
-        
-        
-        
-        
+
+        def = repo.addNewRule("myRule2", "A rule2", "some comment");               
+        System.out.println(def.getId());
+
+        def = repo.addNewRule("myRule3", "A rule3", "some comment");               
+        assertNotNull(def.getDateCreated());
     }
+    
+    public void testListRules() {
+        RepositoryImpl repo = getRepo();
+        repo.addNewRule("blah", "blah", "yeah");
+        List list = repo.listRules(true);
+        assertTrue(list.size() > 0);        
+    }
+    
+    
+    
+    private RepositoryImpl getRepo() {
+        RepositoryImpl repo = new RepositoryImpl();
+        return repo;
+    }
+    
     
 }
