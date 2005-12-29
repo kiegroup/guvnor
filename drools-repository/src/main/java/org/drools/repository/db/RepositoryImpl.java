ffa89060e0183aebb9d37d1c8ad462fa614f4238
Michael Neale <michael.neale@gmail.com>
2005-12-29 08:06:08 +0000

    got basics working
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@1955 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-repository/src/main/java/org/drools/repository/db/RepositoryImpl.java
+++ drools-repository/src/main/java/org/drools/repository/db/RepositoryImpl.java
@@ -1,7 +1,10 @@
 package org.drools.repository.db;
 
+import java.util.List;
+
 import org.drools.repository.Repository;
 import org.drools.repository.RuleDef;
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 
@@ -9,18 +12,32 @@ import org.hibernate.Session;
 public class RepositoryImpl implements Repository {
 
     public RuleDef addNewRule(String ruleName, String content, String comment) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-
+        Session session = getSession();
         session.beginTransaction();
 
-        RuleDef ruleDef = new RuleDef();
-        ruleDef.setContent(content);
-        ruleDef.setName(ruleName);
-
+        RuleDef ruleDef = new RuleDef(ruleName, content);
         session.save(ruleDef);
 
         session.getTransaction().commit();
         return ruleDef;
     }
+
+    
+    public List listRules(boolean head) {
+        Session session = getSession();
+        session.beginTransaction();
+        List results = session
+                    .createQuery("from RuleDef where head = :head")
+                    .setBoolean("head", head).setMaxResults(1000)
+                    .list();        
+        session.getTransaction().commit();
+        return results;        
+    }
+    
+    private Session getSession(){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        return session;
+    }
+    
     
 }
