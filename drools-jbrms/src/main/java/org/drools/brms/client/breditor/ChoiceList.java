2beffcc935ea5ee9726de97b596b7a9a84f17c91
Michael Neale <michael.neale@gmail.com>
2006-10-09 14:37:26 +0000

    filtering of popup suggestions
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@6707 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-jbrms/src/main/java/org/drools/brms/client/breditor/ChoiceList.java
+++ drools-jbrms/src/main/java/org/drools/brms/client/breditor/ChoiceList.java
@@ -3,9 +3,12 @@ package org.drools.brms.client.breditor;
 import java.util.List;
 
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ChangeListener;
 import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.KeyboardListener;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -17,6 +20,7 @@ public class ChoiceList extends PopupPanel {
 
     private ListBox list;    
     private ClickListener okClickListener;
+    private TextBox filter;
     
     public void setOKClickListener(ClickListener listener) {
         this.okClickListener = listener;
@@ -26,17 +30,44 @@ public class ChoiceList extends PopupPanel {
      * Pass in a list of suggestions for the popup lists.
      * Set a click listener to get notified when the OK button is clicked.
      */
-    public ChoiceList(List data) {
+    public ChoiceList(final List data) {
         super( true );
+        
+        
+        filter = new TextBox();
+        filter.addKeyboardListener( new KeyboardListener() {
+
+            public void onKeyDown(Widget arg0,
+                                  char arg1,
+                                  int arg2) {
+                
+            }
+
+            public void onKeyPress(Widget arg0,
+                                   char arg1,
+                                   int arg2) {
+                
+            }
+
+            public void onKeyUp(Widget arg0,
+                                char arg1,
+                                int arg2) {
+                populateList( ListUtil.filter(data, filter.getText()) );
+            }
+            
+        });
+        filter.setFocus( true );
+        
+        
+        VerticalPanel panel = new VerticalPanel();
+        panel.add( filter );
         
         list = new ListBox();
         list.setVisibleItemCount( 5 );
         
-        for (int i = 0; i < data.size(); i++) {
-            list.addItem((String) data.get( i ));
-        }        
+        populateList( data );        
         
-        VerticalPanel panel = new VerticalPanel();
+        
         panel.add( list );
         
         Button ok = new Button("ok");
@@ -50,6 +81,13 @@ public class ChoiceList extends PopupPanel {
         add( panel );      
         setStyleName( "ks-popups-Popup" );
         
+    }
+
+    private void populateList(List data) {
+        list.clear();
+        for (int i = 0; i < data.size(); i++) {
+            list.addItem((String) data.get( i ));
+        }
     }
     
     private void onOkClicked() {        
