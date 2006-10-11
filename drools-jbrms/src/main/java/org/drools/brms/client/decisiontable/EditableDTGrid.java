2c4e9fadbf7455ed7bea56dea88043a5a106f713
Michael Neale <michael.neale@gmail.com>
2006-10-11 19:54:48 +0000

    some decision table stuff
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@6748 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-jbrms/src/main/java/org/drools/brms/client/decisiontable/EditableDTGrid.java
+++ drools-jbrms/src/main/java/org/drools/brms/client/decisiontable/EditableDTGrid.java
@@ -5,26 +5,33 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.SourcesTableEvents;
+import com.google.gwt.user.client.ui.TableListener;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
 public class EditableDTGrid extends Composite {
 
-    private Grid table = new Grid();
+    private FlexTable table = new FlexTable();
 
     public EditableDTGrid() {
 
         //for if I switch to a Grid
-        table.resizeColumns( numCols() + 1 );
-        table.resizeRows( numRows() );
+//        table.resizeColumns( numCols() + 1 );
+//        table.resizeRows( numRows() );
 
+        table.setStyleName( "dt-editor-Grid" );
+        
         for ( int row = 0; row < numRows(); row++ ) {
             int column = 0;
             for ( ; column < numCols(); column++ ) {
                 table.setText( row,
                                column,
                                "boo " + column );
+                table.getFlexCellFormatter().setStyleName( row, column, "dt-editor-Cell" );
             }
+            
             final int currentRow = row;
             Image editButton = new Image("images/edit.gif");
             editButton.addClickListener( new ClickListener() {
@@ -38,8 +45,27 @@ public class EditableDTGrid extends Composite {
             }) ;          
             table.setWidget( row, column, editButton );
             
-        }
 
+            
+        }
+        
+        //the click listener
+        table.addTableListener( new TableListener() {
+
+            public void onCellClicked(SourcesTableEvents events,
+                                      int row,
+                                      int column) {
+                System.out.println("cell clicked, row = " + row);
+                System.out.println("cell clicked, col = " + column);                
+            }
+            
+        });
+        
+        //and this is how you span things
+        //table.getFlexCellFormatter().setColSpan( 2, 3, 4 );
+
+        
+        
         initWidget( table );
 
     }
@@ -66,11 +92,11 @@ public class EditableDTGrid extends Composite {
     }
 
     private int numCols() {
-        return 12;
+        return 7;
     }
 
     private int numRows() {
-        return 70;
+        return 12;
     }
 
 }
