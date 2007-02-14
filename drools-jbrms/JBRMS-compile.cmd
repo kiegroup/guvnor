79d460c137f5445040ac87eb78f004fb16d08bfd
Fernando Meyer <fmcamargo@gmail.com>
2007-02-14 16:31:12 +0000

    deleting some unnecessary files
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@9494 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-jbrms/JBRMS-compile.cmd
+++
@@ -1,1 +0,0 @@
-@java -cp "%~dp0\src\main\java;%~dp0\bin;D:\gwt-windows-1.1.10\gwt-user.jar;D:\gwt-windows-1.1.10\gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\www" %* org.drools.brms.JBRMS
