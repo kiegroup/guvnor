5ecba583ecc9045b5ee9df804d67ab90bd798163
Michael Neale <michael.neale@gmail.com>
2007-02-15 04:24:54 +0000

    cosmetic changes
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@9510 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-jbrms/src/main/java/org/drools/brms/client/Info.java
+++ drools-jbrms/src/main/java/org/drools/brms/client/Info.java
@@ -1,53 +1,37 @@
-/*
- * Copyright 2006 Google Inc.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package org.drools.brms.client;
 
-import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
-
-
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  * Introduction page.
  */
 public class Info extends JBRMSFeature {
 
-  public static ComponentInfo init() {
-    return new ComponentInfo("Info", "JBoss Rules Managment Console.") {
-      public JBRMSFeature createInstance() {
-        return new Info();
-      }
+    public static ComponentInfo init() {
+        return new ComponentInfo( "Info",
+                                  "JBoss Rules Managment Console." ) {
+            public JBRMSFeature createInstance() {
+                return new Info();
+            }
 
-    };
-  }
+        };
+    }
 
-  public Info() {
-	  	
-    initWidget(new HTML(
-      "<div class='infoProse'>"
-        + "Welcome to the JBoss Rules Management System console."
-        + "<p>"
-        + "</div>"
-        + "<div>"
-        + "<img src='images/front_icon.gif'"
-        + "</div>"
-        ,
-      true));
-  }
+    public Info() {
+        initWidget( getLayout() );
+    }
 
-  public void onShow() {
-  }
+    private Widget getLayout() {
+        VerticalPanel horiz = new VerticalPanel();
+        horiz.add( new Image( "images/logo.png" ) );
+        horiz.add( new Label("Welcome to the JBoss Rules Management system.") );
+        return horiz;
+    }
+
+    public void onShow() {
+    }
 }
