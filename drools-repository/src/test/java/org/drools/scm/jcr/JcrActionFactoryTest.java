94935b9f7294e7b4eef0b73f8e3c58c507eae3b6
Mark Proctor <mdproctor@gmail.com>
2007-02-15 13:22:51 +0000

    -made the jcr add directory test available
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@9529 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-repository/src/test/java/org/drools/scm/jcr/JcrActionFactoryTest.java
+++ drools-repository/src/test/java/org/drools/scm/jcr/JcrActionFactoryTest.java
@@ -20,16 +20,20 @@ import junit.framework.TestCase;
 public class JcrActionFactoryTest extends TestCase {
 
     public void testMapPathNameToPackage() {
-        JcrActionFactory fact = new JcrActionFactory(null);
-        assertEquals("org.foo.bar", fact.toPackageName("org/foo/bar"));
-        assertEquals("foo", fact.toPackageName("foo"));
-        assertEquals("FooBar", fact.toPackageName("FooBar"));
-        
-    
-        assertEquals("org/foo/bar", fact.toDirectoryName("org.foo.bar"));
-        assertEquals("foo", fact.toDirectoryName("foo"));
+        JcrActionFactory fact = new JcrActionFactory( null );
+        assertEquals( "org.foo.bar",
+                      fact.toPackageName( "org/foo/bar" ) );
+        assertEquals( "foo",
+                      fact.toPackageName( "foo" ) );
+        assertEquals( "FooBar",
+                      fact.toPackageName( "FooBar" ) );
+
+        assertEquals( "org/foo/bar",
+                      fact.toDirectoryName( "org.foo.bar" ) );
+        assertEquals( "foo",
+                      fact.toDirectoryName( "foo" ) );
     }
-    
+
     public void testAddDirectories() throws Exception {
         ScmActionFactory svn = new JcrActionFactory( RepositorySessionUtil.getRepository() );
 
@@ -38,7 +42,7 @@ public class JcrActionFactoryTest extends TestCase {
         // Correctly add a new Directory at root
         actions = new CompositeScmAction();
         ScmAction addDirectory = new AddDirectory( "",
-                                                "folder1" );
+                                                   "folder1" );
         actions.addScmAction( addDirectory );
 
         svn.execute( actions,
@@ -48,23 +52,23 @@ public class JcrActionFactoryTest extends TestCase {
         actions = new CompositeScmAction();
 
         addDirectory = new AddDirectory( "folder1",
-                                      "folder1_1" );
+                                         "folder1_1" );
         actions.addScmAction( addDirectory );
 
         addDirectory = new AddDirectory( "folder1/folder1_1",
-                                      "folder1_1_1" );
+                                         "folder1_1_1" );
         actions.addScmAction( addDirectory );
 
         addDirectory = new AddDirectory( "folder1",
-                                      "folder1_2" );
+                                         "folder1_2" );
         actions.addScmAction( addDirectory );
 
         addDirectory = new AddDirectory( "",
-                                      "folder2/folder2_1" );
+                                         "folder2/folder2_1" );
         actions.addScmAction( addDirectory );
 
         addDirectory = new AddDirectory( "",
-                                      "folder3/folder3_1/folder3_1_1/folder3_1_1_1" );
+                                         "folder3/folder3_1/folder3_1_1/folder3_1_1_1" );
         actions.addScmAction( addDirectory );
 
         svn.execute( actions,
@@ -80,48 +84,63 @@ public class JcrActionFactoryTest extends TestCase {
         assertTrue( list.contains( "folder2/folder2_1" ) );
         assertTrue( list.contains( "folder3/folder3_1/folder3_1_1/folder3_1_1_1" ) );
     }
-    
+
     public void testAddFiles() throws Exception {
         RulesRepository repo = RepositorySessionUtil.getRepository();
-        
-        repo.createPackage( "testAddFiles.package", "just for testing" );
-        
-        JcrActionFactory fact = new JcrActionFactory(repo);
-        
+
+        repo.createPackage( "testAddFiles.package",
+                            "just for testing" );
+
+        JcrActionFactory fact = new JcrActionFactory( repo );
+
         byte[] data = "this is content".getBytes();
-        ScmAction action = fact.addFile( "testAddFiles/package", "someFile.drl", data );
-        
-        fact.execute( action, "some message" );
-        
+        ScmAction action = fact.addFile( "testAddFiles/package",
+                                         "someFile.drl",
+                                         data );
+
+        fact.execute( action,
+                      "some message" );
+
         PackageItem pk = repo.loadPackage( "testAddFiles.package" );
         AssetItem asset = pk.loadAsset( "someFile" );
-        
-        assertEquals("drl", asset.getFormat());
-        assertEquals("this is content", asset.getContent());
-        assertEquals("some message", asset.getDescription());
-        assertEquals("Draft", asset.getStateDescription());
+
+        assertEquals( "drl",
+                      asset.getFormat() );
+        assertEquals( "this is content",
+                      asset.getContent() );
+        assertEquals( "some message",
+                      asset.getDescription() );
+        assertEquals( "Draft",
+                      asset.getStateDescription() );
     }
-    
+
     public void testUpdateFiles() throws Exception {
         RulesRepository repo = RepositorySessionUtil.getRepository();
         PackageItem pkg = repo.loadDefaultPackage();
-        AssetItem asset = pkg.addAsset( "testUpdateFilesSVN", "something" );
-        
+        AssetItem asset = pkg.addAsset( "testUpdateFilesSVN",
+                                        "something" );
+
         asset.updateContent( "lala" );
         asset.checkin( "yeah" );
         String oldVersion = asset.getVersionNumber();
-        
-        JcrActionFactory fact = new JcrActionFactory(repo);
-        ScmAction action = fact.updateFile( "default", "testUpdateFilesSVN.drl", "lala".getBytes(), "lala2".getBytes() );
 
-        fact.execute( action, "goo" );
-        
+        JcrActionFactory fact = new JcrActionFactory( repo );
+        ScmAction action = fact.updateFile( "default",
+                                            "testUpdateFilesSVN.drl",
+                                            "lala".getBytes(),
+                                            "lala2".getBytes() );
+
+        fact.execute( action,
+                      "goo" );
+
         AssetItem asset2 = pkg.loadAsset( "testUpdateFilesSVN" );
-        assertFalse(oldVersion.equals( asset2.getVersionNumber() ));
-        assertEquals("lala2", asset2.getContent());
-        assertEquals("goo", asset2.getCheckinComment());        
-    }    
-    
+        assertFalse( oldVersion.equals( asset2.getVersionNumber() ) );
+        assertEquals( "lala2",
+                      asset2.getContent() );
+        assertEquals( "goo",
+                      asset2.getCheckinComment() );
+    }
+
     public static List convertToStringList(List list) {
         List files = new ArrayList( list.size() );
 
@@ -130,6 +149,6 @@ public class JcrActionFactoryTest extends TestCase {
             files.add( entry.getPath().equals( "" ) ? entry.getName() : entry.getPath() + "/" + entry.getName() );
         }
         return files;
-    }    
-    
+    }
+
 }
