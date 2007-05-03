afe0a0e6782609bf672ae890720d3ae38c282ea8
Michael Neale <michael.neale@gmail.com>
2007-05-03 08:02:36 +0000

    added another simple test
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@11628 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-jbrms/src/test/java/org/drools/brms/client/common/AssetFormatsTest.java
@@ -0,0 +1,23 @@
+package org.drools.brms.client.common;
+
+import junit.framework.TestCase;
+
+public class AssetFormatsTest extends TestCase {
+
+    
+    public void testGrouping() {
+        String[] formats = AssetFormats.BUSINESS_RULE_FORMATS;
+        for ( int i = 0; i < formats.length; i++ ) {
+            String fmt = formats[i];
+            if (! (fmt.equals( AssetFormats.BUSINESS_RULE )
+                    ||
+                    fmt.equals( AssetFormats.DECISION_SPREADSHEET_XLS )
+                    ||
+                    fmt.equals( AssetFormats.DSL_TEMPLATE_RULE )
+            ) ) {
+                fail("Incorrect grouping of business rules.");
+            }
+        }
+    }
+    
+}
