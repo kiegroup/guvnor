fa54d718c2aafc23d96c7ac417dd778b74b4bcff
Michael Neale <michael.neale@gmail.com>
2008-01-08 05:24:57 +0000

    JBRULES-1393 Analysis tool
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@17604 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-jbrms/src/main/java/org/drools/brms/client/qa/AnalysisResultWidget.java
+++ drools-jbrms/src/main/java/org/drools/brms/client/qa/AnalysisResultWidget.java
@@ -5,6 +5,7 @@ import org.drools.brms.client.rpc.AnalysisReport;
 import org.drools.brms.client.rpc.AnalysisReportLine;
 
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Tree;
 import com.google.gwt.user.client.ui.TreeItem;
 
@@ -15,35 +16,46 @@ import com.google.gwt.user.client.ui.TreeItem;
 public class AnalysisResultWidget extends Composite {
 
 	public AnalysisResultWidget(String packageName, AnalysisReport report) {
-		FormStyleLayout layout = new FormStyleLayout();
+		FormStyleLayout layout = new FormStyleLayout("images/analyse_large.png", "Analysis of package: " + packageName);
 
 		Tree t = new Tree();
-		TreeItem warnings = new TreeItem("Warnings [" + report.warnings.length + "] items.");
 
-		t.addItem(warnings);
-
-
-
-		for (int i = 0; i < report.warnings.length; i++) {
-			AnalysisReportLine r = report.warnings[i];
-			TreeItem w = new TreeItem(r.description);
-			TreeItem reason = new TreeItem("Reason: " + r.reason);
-			w.addItem(reason);
-			TreeItem causes = new TreeItem("Cause:");
-
-			for (int j = 0; j < r.cause.length; j++) {
-				causes.addItem(new TreeItem(r.cause[j]));
-			}
-			if (r.cause.length > 0 ) w.addItem(causes);
-			warnings.addItem(w);
-
-
-		}
+		t.addItem(renderItems(report.errors, "images/error.gif", "Errors"));
+		t.addItem(renderItems(report.warnings, "images/warning.gif", "Warnings"));
+		t.addItem(renderItems(report.notes, "images/note.gif", "Notes"));
 
 		layout.addRow(t);
 
-		warnings.setState(true);
 		initWidget(layout);
+	}
+
+	private TreeItem renderItems(AnalysisReportLine[] lines, String icon, String msg) {
+		if (lines.length == 0) {
+			TreeItem nil = new TreeItem(new HTML("<i>No " + msg + "</i>"));
+			nil.setStyleName("model-builder-Background");
+			return nil;
+		}
+		TreeItem lineNode = new TreeItem(new HTML("<img src='" + icon + "' /> &nbsp;  <b>" + msg + "</b> ("+ lines.length + " items)."));
+
+		lineNode.setStyleName("model-builder-Background");
+
+		for (int i = 0; i < lines.length; i++) {
+			AnalysisReportLine r = lines[i];
+			TreeItem w = new TreeItem(new HTML(r.description));
+			w.addItem(new TreeItem(new HTML("<b>Reason:</b>&nbsp;" + r.reason)));
+			TreeItem causes = new TreeItem(new HTML("<b>Cause:</b>"));
+
+			for (int j = 0; j < r.cause.length; j++) {
+ 				causes.addItem(new HTML(r.cause[j]));
+			}
+			if (r.cause.length > 0 ) {
+				w.addItem(causes);
+				causes.setState(true);
+			}
+			lineNode.addItem(w);
+		}
+		lineNode.setState(true);
+		return lineNode;
 	}
 
 }
