612b5f637f62e8685e20b594e78b1da83184df8e
Michael Neale <michael.neale@gmail.com>
2008-06-10 01:53:48 +0000

    Migrated package configuration from text field on package node to a drools.package file (as it is drools specific)
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@20390 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-repository/src/main/java/org/drools/repository/migration/MigrateDroolsPackage.java
@@ -0,0 +1,58 @@
+package org.drools.repository.migration;
+
+import javax.jcr.RepositoryException;
+
+import org.drools.repository.AssetItem;
+import org.drools.repository.PackageItem;
+import org.drools.repository.PackageIterator;
+import org.drools.repository.RulesRepository;
+
+/**
+ * This is a one time class which will be used to migrate package header info for drools.
+ * Only needed for drools version 4 repositories.
+ * @author Michael Neale
+ */
+public class MigrateDroolsPackage {
+
+	public boolean needsMigration(RulesRepository repo) throws RepositoryException {
+		return !repo.getSession().getRootNode().hasNode("drools.package.migrated");
+	}
+
+	public void migrate(RulesRepository repo) throws RepositoryException {
+		System.out.println("AUTO MIGRATION: Performing drools.package migration...");
+    	PackageIterator pkgs = repo.listPackages();
+    	boolean performed = false;
+    	while(pkgs.hasNext()) {
+    		performed = true;
+    		PackageItem pkg = (PackageItem) pkgs.next();
+    		migratePackage(pkg);
+
+    		String[] snaps = repo.listPackageSnapshots(pkg.getName());
+    		if (snaps != null) {
+	    		for (int i = 0; i < snaps.length; i++) {
+	    			PackageItem snap = repo.loadPackageSnapshot(pkg.getName(), snaps[i]);
+	    			migratePackage(snap);
+				}
+    		}
+    	}
+
+
+
+    	if (performed) {
+	    	repo.getSession().getRootNode().addNode("drools.package.migrated");
+	    	repo.save();
+	    	System.out.println("AUTO MIGRATION: drools.package migration completed.");
+    	}
+	}
+
+	private void migratePackage(PackageItem pkg) {
+		if (!pkg.containsAsset("drools")) {
+			AssetItem asset = pkg.addAsset("drools", "");
+			asset.updateFormat("package");
+			asset.updateContent(pkg.getStringProperty(pkg.HEADER_PROPERTY_NAME));
+			asset.checkin("");
+		}
+	}
+
+
+}
---
+++ drools-repository/src/test/java/org/drools/repository/migration/MigrateDroolsPackageTest.java
@@ -0,0 +1,56 @@
+package org.drools.repository.migration;
+
+import javax.jcr.Session;
+
+import org.drools.repository.AssetItem;
+import org.drools.repository.PackageItem;
+import org.drools.repository.RepositorySessionUtil;
+import org.drools.repository.RulesRepository;
+
+import junit.framework.TestCase;
+
+public class MigrateDroolsPackageTest extends TestCase {
+
+	public void testMigrate() throws Exception {
+		RulesRepository repo = RepositorySessionUtil.getRepository();
+		Session sess = repo.getSession();
+		sess.getRootNode().getNode("drools.package.migrated").remove();
+		sess.save();
+
+		MigrateDroolsPackage mig = new MigrateDroolsPackage();
+
+		PackageItem pkg = repo.createPackage("testMigratePackage", "");
+		pkg.updateStringProperty("some header", pkg.HEADER_PROPERTY_NAME);
+		sess.save();
+
+		repo.createPackageSnapshot("testMigratePackage", "SNAP1");
+		repo.createPackageSnapshot("testMigratePackage", "SNAP2");
+
+
+
+		assertTrue(mig.needsMigration(repo));
+		mig.migrate(repo);
+		assertFalse(repo.getSession().hasPendingChanges());
+		assertFalse(mig.needsMigration(repo));
+		pkg = repo.loadPackage("testMigratePackage");
+		assertTrue(pkg.containsAsset("drools"));
+		AssetItem as = pkg.loadAsset("drools");
+		assertEquals("some header", as.getContent());
+
+
+		pkg = repo.loadPackageSnapshot("testMigratePackage", "SNAP1");
+		assertTrue(pkg.containsAsset("drools"));
+		as = pkg.loadAsset("drools");
+		assertEquals("some header", as.getContent());
+
+		pkg = repo.loadPackageSnapshot("testMigratePackage", "SNAP2");
+		assertTrue(pkg.containsAsset("drools"));
+		as = pkg.loadAsset("drools");
+		assertEquals("some header", as.getContent());
+
+
+
+
+	}
+
+}
