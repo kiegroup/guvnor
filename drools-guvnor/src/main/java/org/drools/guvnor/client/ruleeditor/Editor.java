21ce6c014ec4006c7330ceac54a561bdb453c0e5
Anton Arhipov <ant.arhipov@gmail.com>
2008-07-01 04:40:15 +0000

    the common interface class added for content editors
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@20850 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-guvnor/src/main/java/org/drools/guvnor/client/ruleeditor/Editor.java
@@ -0,0 +1,35 @@
+package org.drools.guvnor.client.ruleeditor;
+
+import org.drools.guvnor.client.rpc.RuleAsset;
+
+/*
+ * Copyright 2008 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/**
+ * A marker interface to indicate the content editor to be included to EditorLauncher code generation
+ *
+ * we can replace this interface with annotation if we upgrade to GWT v1.5
+ *
+ * @author Anton Arhipov
+ */
+public interface Editor {
+
+    String getWrapperClass();
+
+    public String  add();
+
+}
