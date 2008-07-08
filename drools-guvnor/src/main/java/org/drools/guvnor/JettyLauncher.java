19da05ab6934c0698a0c10fe242281f82c916f3f
Anton Arhipov <ant.arhipov@gmail.com>
2008-07-08 18:55:16 +0000

    not supporting jetty6 for now
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@20964 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-guvnor/src/main/java/org/drools/guvnor/JettyLauncher.java
+++ drools-guvnor/src/main/java/org/drools/guvnor/JettyLauncher.java
@@ -34,20 +34,15 @@ public class JettyLauncher {
 
     private Class getJettyServerClassName() throws ClassNotFoundException {
         try {
-            System.out.println("starting jetty6...");
-            return Class.forName("org.mortbay.jetty.Main");// jetty 6
-        } catch (ClassNotFoundException e1) {
-            System.err.println("jetty6 failed: " + e1.getMessage());
-            try {
-                System.out.println("starting jetty5...");
-                return Class.forName("org.mortbay.start.Main");// jetty 5
-            }
-            catch (ClassNotFoundException e2) {
-                System.err.println("jetty5 failed: " + e1.getMessage());
-                System.out.println("starting jetty4...");
-                return Class.forName("org.mortbay.jetty.Server");// jetty 4 and early versions
-            }
+            System.out.println("starting jetty5...");
+            return Class.forName("org.mortbay.jetty.Server"); // jetty 5
         }
+        catch (ClassNotFoundException e) {
+            System.err.println("jetty5 failed: " + e.getMessage());
+            System.out.println("starting jetty4...");
+            return Class.forName("org.mortbay.start.Main");// jetty 4 and early versions
+        }
+
     }
 
     public static void main(String[] args) throws Exception {
