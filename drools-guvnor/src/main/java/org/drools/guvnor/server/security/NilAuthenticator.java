7954c4b8c5b00fd53b1e075e8582664d9784257e
Jervis Liu <jervisliu@gmail.com>
2008-07-17 01:28:50 +0000

    Any authenticator should add everyone as admin by default, this way things work same as before no matter with or without role based authorization. https://jira.jboss.org/jira/browse/JBRULES-1683
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@21073 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-guvnor/src/main/java/org/drools/guvnor/server/security/NilAuthenticator.java
+++ drools-guvnor/src/main/java/org/drools/guvnor/server/security/NilAuthenticator.java
@@ -19,6 +19,7 @@ package org.drools.guvnor.server.security;
 
 import org.apache.log4j.Logger;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.security.Identity;
 
 /**
  * This will let any user in, effectively removing any authentication (as the system 
@@ -32,6 +33,9 @@ public class NilAuthenticator {
     
     public boolean authenticate() {
         log.info( "All users are guests.");
+        //Add everyone as admin.
+		Identity.instance().addRole(RoleTypes.ADMIN);
+
         return true;
     }
 }
