64206ea8b64b40f05de97d64c87ac51d39b9146f
Anton Arhipov <ant.arhipov@gmail.com>
2008-08-12 20:08:41 +0000

    GSOC2008: properties editor
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@21489 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-guvnor/src/main/java/org/drools/guvnor/server/util/PropertiesPersistence.java
+++ drools-guvnor/src/main/java/org/drools/guvnor/server/util/PropertiesPersistence.java
@@ -1,5 +1,19 @@
 package org.drools.guvnor.server.util;
-
+/*
+ * Copyright 2005 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import org.drools.guvnor.client.ruleeditor.PropertiesHolder;
 import org.drools.guvnor.client.ruleeditor.PropertyHolder;
 
@@ -7,7 +21,9 @@ import java.util.List;
 import java.util.ArrayList;
 
 /**
+ * used to convert PropertiesHolder to text and back
  *
+ * @author Anton Arhipov
  */
 public class PropertiesPersistence {
 
