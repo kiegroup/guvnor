b4831826eddb87860e37aecf6d64c15baa9aee96
Toni Rikkola <toni@rikkola.net>
2009-03-02 09:14:36 +0000

    GUVNOR-110: category rules not inherited
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@25469 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-guvnor/src/main/java/org/drools/guvnor/server/contenthandler/DSLRuleContentHandler.java
+++ drools-guvnor/src/main/java/org/drools/guvnor/server/contenthandler/DSLRuleContentHandler.java
@@ -1,4 +1,5 @@
 package org.drools.guvnor.server.contenthandler;
+
 /*
  * Copyright 2005 JBoss Inc
  * 
@@ -14,8 +15,6 @@ package org.drools.guvnor.server.contenthandler;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 
 import java.io.IOException;
 import java.io.StringReader;
@@ -35,18 +34,18 @@ import org.drools.repository.PackageItem;
 
 import com.google.gwt.user.client.rpc.SerializableException;
 
-public class DSLRuleContentHandler extends ContentHandler implements IRuleAsset {
-
+public class DSLRuleContentHandler extends ContentHandler
+    implements
+    IRuleAsset {
 
     public void retrieveAssetContent(RuleAsset asset,
                                      PackageItem pkg,
                                      AssetItem item) throws SerializableException {
         RuleContentText text = new RuleContentText();
         text.content = item.getContent();
-        
 
         asset.content = text;
-        
+
     }
 
     public void storeAssetContent(RuleAsset asset,
@@ -57,58 +56,78 @@ public class DSLRuleContentHandler extends ContentHandler implements IRuleAsset 
 
     }
 
-    public void compile(BRMSPackageBuilder builder, AssetItem asset, ContentPackageAssembler.ErrorLogger logger) throws DroolsParserException,
-                                                                    IOException {
-        DefaultExpander expander = getExpander( builder, asset, logger );
-        
+    public void compile(BRMSPackageBuilder builder,
+                        AssetItem asset,
+                        ContentPackageAssembler.ErrorLogger logger) throws DroolsParserException,
+                                                                   IOException {
+        DefaultExpander expander = getExpander( builder,
+                                                asset,
+                                                logger );
+
         //add the rule keyword if its 'stand alone'
         String source = asset.getContent();
-        if (DRLFileContentHandler.isStandAloneRule(source)) {
-            source = wrapRule( asset, source );
+        if ( DRLFileContentHandler.isStandAloneRule( source ) ) {
+            String parentName = this.parentNameFromCategory( asset,
+                                                             "" );
+            source = wrapRule( asset,
+                               parentName,
+                               source );
         }
-        
+
         //expand and check for errors
         String drl = expander.expand( source );
-        if (expander.hasErrors()) {
+        if ( expander.hasErrors() ) {
             List exErrs = expander.getErrors();
             for ( Iterator iter = exErrs.iterator(); iter.hasNext(); ) {
                 ExpanderException ex = (ExpanderException) iter.next();
-                logger.logError( new ContentAssemblyError(asset, ex.getMessage()));
+                logger.logError( new ContentAssemblyError( asset,
+                                                           ex.getMessage() ) );
             }
             return;
         }
-        
-        
-        builder.addPackageFromDrl( new StringReader(drl) );
+
+        builder.addPackageFromDrl( new StringReader( drl ) );
     }
 
-    private DefaultExpander getExpander(BRMSPackageBuilder builder, AssetItem asset, ContentPackageAssembler.ErrorLogger logger) {
-        
-        if (!builder.hasDSL()) {
-            logger.logError( new ContentAssemblyError(asset, "This rule asset requires a DSL, yet none were configured in the package.") );
+    private DefaultExpander getExpander(BRMSPackageBuilder builder,
+                                        AssetItem asset,
+                                        ContentPackageAssembler.ErrorLogger logger) {
+
+        if ( !builder.hasDSL() ) {
+            logger.logError( new ContentAssemblyError( asset,
+                                                       "This rule asset requires a DSL, yet none were configured in the package." ) );
         }
-        
 
         return builder.getDSLExpander();
     }
 
-    public void assembleDRL(BRMSPackageBuilder builder, AssetItem asset, StringBuffer buf) {
+    public void assembleDRL(BRMSPackageBuilder builder,
+                            AssetItem asset,
+                            StringBuffer buf) {
         //add the rule keyword if its 'stand alone'
         String source = asset.getContent();
-        if (DRLFileContentHandler.isStandAloneRule(source)) {
-            source = wrapRule( asset, source );
+        if ( DRLFileContentHandler.isStandAloneRule( source ) ) {
+            String parentName = this.parentNameFromCategory( asset,
+                                                             "" );
+            source = wrapRule( asset,
+                               parentName,
+                               source );
         }
-        
-        DefaultExpander expander = builder.getDSLExpander();    
+
+        DefaultExpander expander = builder.getDSLExpander();
         buf.append( expander.expand( source ) );
-        
+
     }
 
-    private String wrapRule(AssetItem asset, String source) {
-        return "rule '" + asset.getName() + "' \n" + source + "\nend";
+    private String wrapRule(AssetItem asset,
+                            String parentName,
+                            String source) {
+        if ( parentName == null || "".equals( parentName ) ) {
+            return "rule '" + asset.getName() + "' \n" + source + "\nend";
+        } else {
+            return "rule '" + asset.getName() + "' extends " + parentName + " \n" + source + "\nend";
+
+        }
     }
-
-
-    
 
 }
--- drools-guvnor/src/test/java/org/drools/guvnor/client/modeldriven/ui/ConstraintValueEditorTest.java
+++ drools-guvnor/src/test/java/org/drools/guvnor/client/modeldriven/ui/ConstraintValueEditorTest.java
@@ -1,15 +1,15 @@
 package org.drools.guvnor.client.modeldriven.ui;
 
-import org.drools.guided.ConstraintValueEditorHelper;
-
 import junit.framework.TestCase;
 
 public class ConstraintValueEditorTest extends TestCase {
 
     public void testSplit() {
         String[] res = ConstraintValueEditorHelper.splitValue( "M=Male" );
-        assertEquals("M", res[0]);
-        assertEquals("Male", res[1]);
+        assertEquals( "M",
+                      res[0] );
+        assertEquals( "Male",
+                      res[1] );
     }
 
 }
