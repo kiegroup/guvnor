bbdb602ad893c95a7b749acb421a2205e83890d4
Toni Rikkola <toni@rikkola.net>
2010-07-27 12:53:21 +0000

    [#GUVNOR-573] Can't add multiple field contraint
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@34216 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-guvnor/src/main/java/org/drools/guvnor/client/modeldriven/ui/factPattern/PopupCreator.java
+++ drools-guvnor/src/main/java/org/drools/guvnor/client/modeldriven/ui/factPattern/PopupCreator.java
@@ -32,6 +32,8 @@ import org.drools.ide.common.client.modeldriven.brl.SingleFieldConstraint;
 import org.drools.ide.common.client.modeldriven.brl.SingleFieldConstraintEBLeftSide;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
@@ -44,11 +46,11 @@ import com.gwtext.client.util.Format;
 
 public class PopupCreator {
 
-    private FactPattern pattern;
+    private FactPattern                pattern;
     private SuggestionCompletionEngine completions;
-    private RuleModeller modeller;
-    private boolean bindable;
-    private Constants constants = ((Constants) GWT.create(Constants.class));
+    private RuleModeller               modeller;
+    private boolean                    bindable;
+    private Constants                  constants = ((Constants) GWT.create( Constants.class ));
 
     /**
      * Returns the pattern.
@@ -109,37 +111,47 @@ public class PopupCreator {
     /**
      * Display a little editor for field bindings.
      */
-    public void showBindFieldPopup(final Widget w, final SingleFieldConstraint con, String[] fields, final PopupCreator popupCreator) {
+    public void showBindFieldPopup(final Widget w,
+                                   final SingleFieldConstraint con,
+                                   String[] fields,
+                                   final PopupCreator popupCreator) {
         final FormStylePopup popup = new FormStylePopup();
-        popup.setWidth(500);
+        popup.setWidth( 500 );
         final HorizontalPanel vn = new HorizontalPanel();
         final TextBox varName = new TextBox();
-        final Button ok = new Button(constants.Set());
-        vn.add(varName);
-        vn.add(ok);
+        final Button ok = new Button( constants.Set() );
+        vn.add( varName );
+        vn.add( ok );
 
-        ok.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
+        ok.addClickHandler( new ClickHandler() {
+            public void onClick(ClickEvent event) {
                 String var = varName.getText();
-                if (modeller.isVariableNameUsed(var)) {
-                    Window.alert(Format.format(constants.TheVariableName0IsAlreadyTaken(), var));
+                if ( modeller.isVariableNameUsed( var ) ) {
+                    Window.alert( Format.format( constants.TheVariableName0IsAlreadyTaken(),
+                                                 var ) );
                     return;
                 }
-                con.setFieldBinding(var);
+                con.setFieldBinding( var );
                 modeller.refreshWidget();
                 popup.hide();
             }
-        });
-        popup.addAttribute(Format.format(constants.BindTheFieldCalled0ToAVariable(), con.getFieldName()), vn);
-        if (fields != null) {
-            Button sub = new Button(constants.ShowSubFields());
-            popup.addAttribute(Format.format(constants.ApplyAConstraintToASubFieldOf0(), con.getFieldName()), sub);
-            sub.addClickHandler(new ClickHandler() {
-    			public void onClick(ClickEvent event) {
+        } );
+        popup.addAttribute( Format.format( constants.BindTheFieldCalled0ToAVariable(),
+                                           con.getFieldName() ),
+                            vn );
+        if ( fields != null ) {
+            Button sub = new Button( constants.ShowSubFields() );
+            popup.addAttribute( Format.format( constants.ApplyAConstraintToASubFieldOf0(),
+                                               con.getFieldName() ),
+                                sub );
+            sub.addClickHandler( new ClickHandler() {
+                public void onClick(ClickEvent event) {
                     popup.hide();
-                    popupCreator.showPatternPopup(w, con.getFieldType(), con);
+                    popupCreator.showPatternPopup( w,
+                                                   con.getFieldType(),
+                                                   con );
                 }
-            });
+            } );
         }
 
         popup.show();
@@ -148,51 +160,56 @@ public class PopupCreator {
     /**
      * This shows a popup for adding fields to a composite
      */
-    public void showPatternPopupForComposite(Widget w, final CompositeFieldConstraint composite) {
-        final FormStylePopup popup = new FormStylePopup("images/newex_wiz.gif", //NON-NLS
-                constants.AddFieldsToThisConstraint());
+    public void showPatternPopupForComposite(Widget w,
+                                             final CompositeFieldConstraint composite) {
+        final FormStylePopup popup = new FormStylePopup( "images/newex_wiz.gif", //NON-NLS
+                                                         constants.AddFieldsToThisConstraint() );
 
         final ListBox box = new ListBox();
-        box.addItem("...");
-        String[] fields = this.completions.getFieldCompletions(this.pattern.factType);
-        for (int i = 0; i < fields.length; i++) {
-            box.addItem(fields[i]);
+        box.addItem( "..." );
+        String[] fields = this.completions.getFieldCompletions( this.pattern.factType );
+        for ( int i = 0; i < fields.length; i++ ) {
+            box.addItem( fields[i] );
         }
 
-        box.setSelectedIndex(0);
+        box.setSelectedIndex( 0 );
 
-        box.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-                composite.addConstraint(new SingleFieldConstraint(box.getItemText(box.getSelectedIndex())));
+        box.addClickHandler( new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                composite.addConstraint( new SingleFieldConstraint( box.getItemText( box.getSelectedIndex() ) ) );
                 modeller.refreshWidget();
                 popup.hide();
             }
-        });
-        popup.addAttribute(constants.AddARestrictionOnAField(), box);
-
+        } );
+        popup.addAttribute( constants.AddARestrictionOnAField(),
+                            box );
 
         final ListBox composites = new ListBox();
-        composites.addItem("..."); //NON-NLS
-        composites.addItem(constants.AllOfAnd(), CompositeFieldConstraint.COMPOSITE_TYPE_AND);
-        composites.addItem(constants.AnyOfOr(), CompositeFieldConstraint.COMPOSITE_TYPE_OR);
-        composites.setSelectedIndex(0);
+        composites.addItem( "..." ); //NON-NLS
+        composites.addItem( constants.AllOfAnd(),
+                            CompositeFieldConstraint.COMPOSITE_TYPE_AND );
+        composites.addItem( constants.AnyOfOr(),
+                            CompositeFieldConstraint.COMPOSITE_TYPE_OR );
+        composites.setSelectedIndex( 0 );
 
-        composites.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
+        composites.addClickHandler( new ClickHandler() {
+            public void onClick(ClickEvent event) {
                 CompositeFieldConstraint comp = new CompositeFieldConstraint();
-                comp.compositeJunctionType = composites.getValue(composites.getSelectedIndex());
-                composite.addConstraint(comp);
+                comp.compositeJunctionType = composites.getValue( composites.getSelectedIndex() );
+                composite.addConstraint( comp );
                 modeller.refreshWidget();
                 popup.hide();
             }
-        });
+        } );
 
-        InfoPopup infoComp = new InfoPopup(constants.MultipleFieldConstraints(), constants.MultipleConstraintsTip());
+        InfoPopup infoComp = new InfoPopup( constants.MultipleFieldConstraints(),
+                                            constants.MultipleConstraintsTip() );
 
         HorizontalPanel horiz = new HorizontalPanel();
-        horiz.add(composites);
-        horiz.add(infoComp);
-        popup.addAttribute(constants.MultipleFieldConstraint(), horiz);
+        horiz.add( composites );
+        horiz.add( infoComp );
+        popup.addAttribute( constants.MultipleFieldConstraint(),
+                            horiz );
 
         popup.show();
 
@@ -201,89 +218,101 @@ public class PopupCreator {
     /**
      * This shows a popup allowing you to add field constraints to a pattern (its a popup).
      */
-    public void showPatternPopup(Widget w, final String factType, final FieldConstraint con) {
+    public void showPatternPopup(Widget w,
+                                 final String factType,
+                                 final FieldConstraint con) {
 
-        String title = (con == null) ? Format.format(constants.ModifyConstraintsFor0(), factType) : constants.AddSubFieldConstraint();
-        final FormStylePopup popup = new FormStylePopup("images/newex_wiz.gif", //NON-NLS
-                title);
+        String title = (con == null) ? Format.format( constants.ModifyConstraintsFor0(),
+                                                      factType ) : constants.AddSubFieldConstraint();
+        final FormStylePopup popup = new FormStylePopup( "images/newex_wiz.gif", //NON-NLS
+                                                         title );
 
         final ListBox box = new ListBox();
-        box.addItem("...");
+        box.addItem( "..." );
         String[] fields = this.completions.getFieldCompletions( FieldAccessorsAndMutators.ACCESSOR,
                                                                 factType );
-        for (int i = 0; i < fields.length; i++) {
-            box.addItem(fields[i]);
+        for ( int i = 0; i < fields.length; i++ ) {
+            box.addItem( fields[i] );
         }
 
-        box.setSelectedIndex(0);
+        box.setSelectedIndex( 0 );
 
-        box.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-                String fieldName = box.getItemText(box.getSelectedIndex());
+        box.addClickHandler( new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                String fieldName = box.getItemText( box.getSelectedIndex() );
                 String qualifiedName = factType + "." + fieldName;
-                String fieldType = completions.getFieldType(qualifiedName);
-                pattern.addConstraint(new SingleFieldConstraint(fieldName, fieldType, con));
+                String fieldType = completions.getFieldType( qualifiedName );
+                pattern.addConstraint( new SingleFieldConstraint( fieldName,
+                                                                  fieldType,
+                                                                  con ) );
                 modeller.refreshWidget();
                 popup.hide();
             }
-        });
-        popup.addAttribute(constants.AddARestrictionOnAField(), box);
+        } );
+        popup.addAttribute( constants.AddARestrictionOnAField(),
+                            box );
 
         final ListBox composites = new ListBox();
-        composites.addItem("...");
-        composites.addItem(constants.AllOfAnd(), CompositeFieldConstraint.COMPOSITE_TYPE_AND);
-        composites.addItem(constants.AnyOfOr(), CompositeFieldConstraint.COMPOSITE_TYPE_OR);
-        composites.setSelectedIndex(0);
+        composites.addItem( "..." );
+        composites.addItem( constants.AllOfAnd(),
+                            CompositeFieldConstraint.COMPOSITE_TYPE_AND );
+        composites.addItem( constants.AnyOfOr(),
+                            CompositeFieldConstraint.COMPOSITE_TYPE_OR );
+        composites.setSelectedIndex( 0 );
 
-        composites.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
+        composites.addChangeHandler( new ChangeHandler() {
+            public void onChange(ChangeEvent event) {
                 CompositeFieldConstraint comp = new CompositeFieldConstraint();
-                comp.compositeJunctionType = composites.getValue(composites.getSelectedIndex());
-                pattern.addConstraint(comp);
+                comp.compositeJunctionType = composites.getValue( composites.getSelectedIndex() );
+                pattern.addConstraint( comp );
                 modeller.refreshWidget();
                 popup.hide();
             }
-        });
+        } );
 
-        InfoPopup infoComp = new InfoPopup(constants.MultipleFieldConstraints(), constants.MultipleConstraintsTip1());
+        InfoPopup infoComp = new InfoPopup( constants.MultipleFieldConstraints(),
+                                            constants.MultipleConstraintsTip1() );
 
         HorizontalPanel horiz = new HorizontalPanel();
 
-        horiz.add(composites);
-        horiz.add(infoComp);
-        if (con == null) {
-            popup.addAttribute(constants.MultipleFieldConstraint(), horiz);
+        horiz.add( composites );
+        horiz.add( infoComp );
+        if ( con == null ) {
+            popup.addAttribute( constants.MultipleFieldConstraint(),
+                                horiz );
         }
 
-        if (con == null) {
-            popup.addRow(new SmallLabel("<i>" + constants.AdvancedOptionsColon() + "</i>")); //NON-NLS
-            Button predicate = new Button(constants.NewFormula());
-            predicate.addClickHandler(new ClickHandler() {
-    			public void onClick(ClickEvent event) {
+        if ( con == null ) {
+            popup.addRow( new SmallLabel( "<i>" + constants.AdvancedOptionsColon() + "</i>" ) ); //NON-NLS
+            Button predicate = new Button( constants.NewFormula() );
+            predicate.addClickHandler( new ClickHandler() {
+                public void onClick(ClickEvent event) {
                     SingleFieldConstraint con = new SingleFieldConstraint();
-                    con.setConstraintValueType(SingleFieldConstraint.TYPE_PREDICATE);
-                    pattern.addConstraint(con);
+                    con.setConstraintValueType( SingleFieldConstraint.TYPE_PREDICATE );
+                    pattern.addConstraint( con );
                     modeller.refreshWidget();
                     popup.hide();
                 }
-            });
-            popup.addAttribute(constants.AddANewFormulaStyleExpression(), predicate);
-            
-            Button ebBtn = new Button(constants.ExpressionEditor());
-            
-            ebBtn.addClickHandler(new ClickHandler() {
-    			public void onClick(ClickEvent event) {
+            } );
+            popup.addAttribute( constants.AddANewFormulaStyleExpression(),
+                                predicate );
+
+            Button ebBtn = new Button( constants.ExpressionEditor() );
+
+            ebBtn.addClickHandler( new ClickHandler() {
+                public void onClick(ClickEvent event) {
                     SingleFieldConstraintEBLeftSide con = new SingleFieldConstraintEBLeftSide();
-                    con.setConstraintValueType(SingleFieldConstraint.TYPE_UNDEFINED);
-                    pattern.addConstraint(con);
-                    con.setExpressionLeftSide(new ExpressionFormLine(new ExpressionUnboundFact(pattern)));
+                    con.setConstraintValueType( SingleFieldConstraint.TYPE_UNDEFINED );
+                    pattern.addConstraint( con );
+                    con.setExpressionLeftSide( new ExpressionFormLine( new ExpressionUnboundFact( pattern ) ) );
                     modeller.refreshWidget();
                     popup.hide();
                 }
-            });
-            popup.addAttribute(constants.ExpressionEditor(), ebBtn);
-                        
-            doBindingEditor(popup);
+            } );
+            popup.addAttribute( constants.ExpressionEditor(),
+                                ebBtn );
+
+            doBindingEditor( popup );
         }
 
         popup.show();
@@ -296,34 +325,36 @@ public class PopupCreator {
      * not be editable.
      */
     private void doBindingEditor(final FormStylePopup popup) {
-        if (bindable && !(modeller.getModel().isBoundFactUsed(pattern.boundName))) {
+        if ( bindable && !(modeller.getModel().isBoundFactUsed( pattern.boundName )) ) {
             HorizontalPanel varName = new HorizontalPanel();
             final TextBox varTxt = new TextBox();
-            if (pattern.boundName == null) {
-                varTxt.setText("");
+            if ( pattern.boundName == null ) {
+                varTxt.setText( "" );
             } else {
-                varTxt.setText(pattern.boundName);
+                varTxt.setText( pattern.boundName );
             }
 
-            varTxt.setVisibleLength(6);
-            varName.add(varTxt);
+            varTxt.setVisibleLength( 6 );
+            varName.add( varTxt );
 
-            Button bindVar = new Button(constants.Set());
-            bindVar.addClickHandler(new ClickHandler() {
-    			public void onClick(ClickEvent event) {
+            Button bindVar = new Button( constants.Set() );
+            bindVar.addClickHandler( new ClickHandler() {
+                public void onClick(ClickEvent event) {
                     String var = varTxt.getText();
-                    if (modeller.isVariableNameUsed(var)) {
-                        Window.alert(Format.format(constants.TheVariableName0IsAlreadyTaken(), var));
+                    if ( modeller.isVariableNameUsed( var ) ) {
+                        Window.alert( Format.format( constants.TheVariableName0IsAlreadyTaken(),
+                                                     var ) );
                         return;
                     }
                     pattern.boundName = varTxt.getText();
                     modeller.refreshWidget();
                     popup.hide();
                 }
-            });
+            } );
 
-            varName.add(bindVar);
-            popup.addAttribute(constants.VariableName(), varName);
+            varName.add( bindVar );
+            popup.addAttribute( constants.VariableName(),
+                                varName );
 
         }
     }
