136ffc0be3d8b3c1f0d937b66e1181a486e27ad8
Kurt T Stam <kstam@redhat.com>
2010-07-29 23:58:09 +0000

    JBRULES-2417, adding a version and referencing an existing namespace url
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@34367 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-guvnor/src/main/java/org/drools/guvnor/server/files/PackageDeploymentServlet.java
+++ drools-guvnor/src/main/java/org/drools/guvnor/server/files/PackageDeploymentServlet.java
@@ -180,9 +180,7 @@ public class PackageDeploymentServlet extends RepositoryServlet {
 		                                                                     "" );
 		                fileName = "ChangeSet.xml";
 		                String xml = "";
-		                xml += "<change-set xmlns='http://drools.org/drools-5.0/change-set'\n";
-		                xml += "    xmlns:xs='http://www.w3.org/2001/XMLSchema-instance'\n";
-		                xml += "    xs:schemaLocation='http://drools.org/drools-5.0/change-set drools-change-set-5.0.xsd' >\n";
+		                xml += "<change-set xmlns='http://anonsvn.jboss.org/repos/labs/labs/jbossrules/trunk/drools-api/src/main/resources/change-set-1.0.0.xsd' >\n";
 		                xml += "    <add>\n ";
 		                xml += "        <resource source='" + url + "' type='PKG' />\n";
 		                xml += "    </add>\n";
