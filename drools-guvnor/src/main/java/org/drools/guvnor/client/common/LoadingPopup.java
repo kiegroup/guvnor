b4f5228c659835458671980d9288ff5903249369
Toni Rikkola <toni@rikkola.net>
2010-09-10 10:13:59 +0000

    GUVNOR-599 : Replace gwtext codes used by Popup related widgets -Loading popup
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@35097 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-guvnor/src/main/java/org/drools/guvnor/client/common/LoadingPopup.java
+++ drools-guvnor/src/main/java/org/drools/guvnor/client/common/LoadingPopup.java
@@ -1,20 +1,3 @@
-/**
- * Copyright 2010 JBoss Inc
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.drools.guvnor.client.common;
 /*
  * Copyright 2005 JBoss Inc
  *
@@ -31,59 +14,37 @@ package org.drools.guvnor.client.common;
  * limitations under the License.
  */
 
+package org.drools.guvnor.client.common;
 
-
-import com.gwtext.client.widgets.MessageBox;
-import com.gwtext.client.widgets.MessageBoxConfig;
-import com.google.gwt.core.client.GWT;
-import org.drools.guvnor.client.messages.Constants;
-
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PopupPanel;
 
 /**
  * Generic "busy" dialog popup.
  * This is a lazy singleton, only really need one to be shown at time.
  */
-public class LoadingPopup  {
+public class LoadingPopup extends PopupPanel {
 
-    public static LoadingPopup instance = null;
+    private static LoadingPopup instance     = new LoadingPopup();
 
-
+    private final Label         messageLabel = new Label();
 
     private LoadingPopup() {
-    }
+        add( messageLabel );
 
-
-    /**
-     * Close the single instance of this dialog...
-     */
-    public static void close() {
-        MessageBox.hide();
-    }
-
-
-
-    public static LoadingPopup getInstance() {
-        if (instance == null) {
-            instance = new LoadingPopup();
-        }
-        return instance;
+        setWidth( "200px" );
+        center();
+        setGlassEnabled( true );
     }
 
     /** Convenience method to popup the message. */
     public static void showMessage(final String message) {
-    	MessageBox.show(new MessageBoxConfig() {
-    		{
-    			setTitle(((Constants) GWT.create(Constants.class)).PleaseWaitDotDotDot());
-    			setWidth(200);
-    			setMsg(message);
-    			setClosable(true);
-    		}
-    	});
+        instance.messageLabel.setText( message );
+        instance.show();
     }
 
-
-
-
-
+    public static void close() {
+        instance.hide();
+    }
 
 }
