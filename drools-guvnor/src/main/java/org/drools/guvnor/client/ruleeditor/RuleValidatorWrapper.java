c4ec87ce68081aac53c37313aebd5b2c60ff5d82
Toni Rikkola <toni@rikkola.net>
2010-10-01 09:28:32 +0000

    GUVNOR-586 : GWTExt replacement -I took away some commented code that still had gwtext in them
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@35378 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-guvnor/src/main/java/org/drools/guvnor/client/ruleeditor/RuleValidatorWrapper.java
+++ drools-guvnor/src/main/java/org/drools/guvnor/client/ruleeditor/RuleValidatorWrapper.java
@@ -1,21 +1,3 @@
-/**
- * Copyright 2010 JBoss Inc
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.drools.guvnor.client.ruleeditor;
-
 /*
  * Copyright 2005 JBoss Inc
  *
@@ -31,6 +13,8 @@ package org.drools.guvnor.client.ruleeditor;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.drools.guvnor.client.ruleeditor;
 
 import org.drools.guvnor.client.common.DirtyableComposite;
 import org.drools.guvnor.client.common.FormStylePopup;
@@ -69,76 +53,12 @@ public class RuleValidatorWrapper extends DirtyableComposite
         this.editor = editor;
 
         layout.add( editor );
-        /*        if (!asset.isreadonly) {
-                	validatorActions();
-                }*/
 
         layout.setWidth( "100%" );
         layout.setHeight( "100%" );
 
         initWidget( layout );
     }
-
-    //This is duplicate to ActionToolbar    
-    /*    private void validatorActions() {
-            Toolbar tb = new Toolbar();
-
-            layout.setCellHeight(editor, "95%");
-            layout.add(tb);
-
-            ToolbarButton viewSource = new ToolbarButton();
-            viewSource.setText(constants.ViewSource());
-            viewSource.addListener(new ButtonListenerAdapter()  {
-    			public void onClick(
-    					com.gwtext.client.widgets.Button button,
-    					EventObject e) {
-                    doViewsource();
-    			}
-    		});
-
-            //only show this for advanced users
-            if (ExplorerLayoutManager.shouldShow(Capabilities.SHOW_PACKAGE_VIEW)) {
-                tb.addButton(viewSource);
-                tb.addSeparator();
-            }
-
-            ToolbarButton validate = new ToolbarButton();
-            validate.setText(constants.Validate());
-            validate.addListener(new ButtonListenerAdapter()  {
-            			public void onClick(
-            					com.gwtext.client.widgets.Button button,
-            					EventObject e) {
-            				doValidate();
-            			}
-            		});
-            tb.addButton(validate);
-
-
-        }
-
-        private void doValidate() {
-        	onSave();
-            LoadingPopup.showMessage(constants.ValidatingItemPleaseWait());
-            RepositoryServiceFactory.getService().buildAsset( asset, new GenericCallback<BuilderResult>() {
-                public void onSuccess(BuilderResult result) {showBuilderErrors(result);}
-            });
-
-        }
-
-        private void doViewsource() {
-        	onSave();
-            LoadingPopup.showMessage(constants.CalculatingSource());
-            RepositoryServiceFactory.getService().buildAssetSource( this.asset, new GenericCallback<String>() {
-                public void onSuccess(String src) { showSource(src);}
-            });
-
-        }
-
-        private void showSource(String src) {
-            PackageBuilderWidget.showSource( src, this.asset.metaData.name );
-            LoadingPopup.close();
-        }
-        */
 
     /**
      * This will show a popup of error messages in compilation.
