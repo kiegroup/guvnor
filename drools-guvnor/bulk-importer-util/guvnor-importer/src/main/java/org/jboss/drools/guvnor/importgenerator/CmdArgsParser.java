a56b552dbc48a9cc854ce6f19d37fe32dcd61a48
Tihomir Surdilovic <tsurdilo@redhat.com>
2010-10-13 19:21:55 +0000

    GUVNOR-672 - bulk importer - allow to pass parameter to executable jar which takes in path to a properties file with config options
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@35514 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-guvnor/bulk-importer-util/guvnor-importer/src/main/java/org/jboss/drools/guvnor/importgenerator/CmdArgsParser.java
+++ drools-guvnor/bulk-importer-util/guvnor-importer/src/main/java/org/jboss/drools/guvnor/importgenerator/CmdArgsParser.java
@@ -16,8 +16,12 @@
 
 package org.jboss.drools.guvnor.importgenerator;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * Command line argument parser
@@ -67,22 +71,44 @@ public class CmdArgsParser{
   
   public Map<Parameters, String> parse(String[] args){
     if (args.length==0){
-      args=new String[]{ //default arguments
-          "-classpath",
-          "-p", "/home/mallen/workspace/guvnor-importer/my_rules",
-          "-s", "rules",
-          "-e", "[0-9|.]*[.|-]+[SNAPSHOT]+[.|-]*[09|.]*",
-          "-r", "true",
-          "-u","admin",
-          "-f","drl,xls",
-          "-o","generated.xml",
-          "-n","1.0.0-SNAPSHOT",
-          "-c","functions.drl",
-          "-k", "http://localhost:8080/brms/org.drools.guvnor.Guvnor/package/",
-          "-b", "/home/mallen/workspace/guvnor-importer",
-          "-w", "kagentChangeSet.xml",
-          "-V"};
-    }
+//      args=new String[]{ //default arguments
+//          "-classpath",
+//          "-p", "/home/mallen/workspace/guvnor-importer/my_rules",
+//          "-s", "rules",
+//          "-e", "[0-9|.]*[.|-]+[SNAPSHOT]+[.|-]*[09|.]*",
+//          "-r", "true",
+//          "-u","admin",
+//          "-f","drl,xls",
+//          "-o","generated.xml",
+//          "-n","1.0.0-SNAPSHOT",
+//          "-c","functions.drl",
+//          "-k", "http://localhost:8080/brms/org.drools.guvnor.Guvnor/package/",
+//          "-b", "/home/mallen/workspace/guvnor-importer",
+//          "-w", "kagentChangeSet.xml",
+//          "-V"};
+    	System.out.println("Invalid number of parameters - 0");
+    	return options;
+    } else if (args.length == 2) {
+    	String arg = args[0];
+    	String val = args[1];
+    	if(arg.equals("-prop")) {
+    		try {
+    			Properties props = new Properties();
+    			props.load(new FileInputStream(val));
+    			for(Object prop : props.keySet()) {
+    				String key = (String) prop;
+    				String value = props.getProperty(key);
+    				options.put(map.get(key), value);
+    			}
+    		} catch (IOException e) {
+    			System.out.println("Invalid file specified: " + val);
+    		}
+    		options.put(Parameters.OPTIONS_VERBOSE, "true");
+    	} else {
+    		System.out.println("To use a properties file use the \"-prop\" parameter");
+    	}
+    	return options;
+    } else {
     for (int i=0;i<args.length;i++) {
       String arg = args[i];
       if (arg.equalsIgnoreCase("-classpath"))
@@ -102,5 +128,6 @@ public class CmdArgsParser{
       System.out.println("   "+key.name()+"="+options.get(key));
     }
     return options;
+  }
   }
 }
