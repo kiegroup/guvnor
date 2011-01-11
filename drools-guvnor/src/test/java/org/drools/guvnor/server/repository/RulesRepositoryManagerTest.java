50be8a9b428ff7b16b2381157266e4a6e97e6969
Toni Rikkola <toni@rikkola.net>
2011-01-11 15:20:47 +0200

    Fixing broken tests in hudson

--- drools-guvnor/src/test/java/org/drools/guvnor/server/repository/RulesRepositoryManagerTest.java
+++ drools-guvnor/src/test/java/org/drools/guvnor/server/repository/RulesRepositoryManagerTest.java
@@ -1,5 +1,5 @@
-/**
- * Copyright 2010 JBoss Inc
+/*
+ * Copyright 2005 JBoss Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,21 +15,6 @@
  */
 
 package org.drools.guvnor.server.repository;
-/*
- * Copyright 2005 JBoss Inc
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -37,31 +22,36 @@ import static org.junit.Assert.assertFalse;
 
 import javax.jcr.Repository;
 
+import org.drools.guvnor.server.util.TestEnvironmentSessionHelper;
 import org.drools.repository.RulesRepositoryConfigurator;
+import org.junit.After;
 import org.junit.Test;
 
 public class RulesRepositoryManagerTest {
 
-	@Test 
+    @Test
     public void testDecorator() {
         RulesRepositoryManager dec = new RulesRepositoryManager();
         RepositoryStartupService config = new RepositoryStartupService();
-        config.properties.put(RulesRepositoryConfigurator.CONFIGURATOR_CLASS,"org.drools.repository.jackrabbit.JackrabbitRepositoryConfigurator");
+        config.properties.put( RulesRepositoryConfigurator.CONFIGURATOR_CLASS,
+                               "org.drools.repository.jackrabbit.JackrabbitRepositoryConfigurator" );
         Repository repo = config.getRepositoryInstance();
         config.repository = repo;
         dec.repositoryConfiguration = config;
 
         dec.create();
-        
-        assertNotNull(dec.getRepository().getSession());
-        
-        
-        assertTrue(dec.getRepository().getSession().isLive());
+
+        assertNotNull( dec.getRepository().getSession() );
+
+        assertTrue( dec.getRepository().getSession().isLive() );
         dec.close();
-        assertFalse(dec.getRepository().getSession().isLive());
-        
-        
+        assertFalse( dec.getRepository().getSession().isLive() );
+
     }
-    
-    
+
+    @After
+    public void tearDown() throws Exception {
+        TestEnvironmentSessionHelper.shutdown();
+    }
+
 }
