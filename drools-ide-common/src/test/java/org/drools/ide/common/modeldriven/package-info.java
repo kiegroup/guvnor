30592e70ef037268c807bf20a1599f44f02c2bdd
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2011-01-28 11:24:21 +0100

    convert package.html into package-info.java

---
+++ drools-ide-common/src/test/java/org/drools/ide/common/modeldriven/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This contains utilities and widgets for the model driven/scorecard style way of editing a rule.
+ * <p/>
+ * This has the interesting feature of being able to work over standard DRL, no special markup needed.
+ * Its actually a subset of DRL, complex structures and semantics can not be supported.
+ * <p/>
+ * However, this can be augmented with DSLs which can express any construct needed.
+ */
+package org.drools.ide.common.modeldriven;
