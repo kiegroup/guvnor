a98ccb14258f9ff26cf02fa291bfd9550b84ba8a
nicolas-sanchez <sancheznicolas89@gmail.com>
2011-01-31 14:48:42 -0300

    SpringContextValidatorTest improvement, Test of 4 cases of validation.

--- drools-guvnor/src/test/java/org/drools/guvnor/client/ruleeditor/SpringContextValidatorTest.java
+++ drools-guvnor/src/test/java/org/drools/guvnor/client/ruleeditor/SpringContextValidatorTest.java
@@ -5,6 +5,7 @@ package org.drools.guvnor.client.ruleeditor;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotSame;
 
 import org.drools.guvnor.client.common.AssetFormats;
 import org.junit.Ignore;
@@ -50,7 +51,56 @@ public class SpringContextValidatorTest {
 								"<property name=\"description\" value=\"A sample description here\"/>"+
 								"</bean>"+
 								"</beans>";
+			
+			String droolsSpringCtxtErr1 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+									  "<beans xmlns=\"http://www.springframework.org/schema/beans\"\n"+
+			                          "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"+
+			                          "xmlns:context=\"http://www.springframework.org/schema/context\"\n"+
+			                          "xmlns:drools=\"http://drools.org/schema/drools-spring\"\n"+
+			                          "xsi:schemaLocation=\"http://www.springframework.org/schema/beans\n"+
+			                          " http://www.springframework.org/schema/beans/spring-beans-2.5.xsd\n"+
+			                          " http://www.springframework.org/schema/context\n"+
+			                          " http://www.springframework.org/schema/context/spring-context-2.5.xsd\n"+
+			                          " http://drools.org/schema/drools-spring\n"+
+			                          " http://drools.org/schema/drools-spring.xsd\""+ 
+			                          " default-autowire=\"byName\">" +
+			                          "<drools:connection id=\"connection1\" type=\"local\" />"+
+			                          "<drools:execution-node id=\"node1\" connection=\"connection1\" />"+
+			                          "<drools:kbase id=\"kbase1\" node=\"node1\">"+
+			                          "<drools:resource source=\"classpath:changesets/change-set-1.xml\" type=\"CHANGE_SET\" />"+
+			                          "<drools:model source=\"classpath:model/person.xsd\" />"+
+			                          "</drools:kbase>"+
+			                          "<drools:kbase id=\"kbase2\" node=\"node1\">"+
+			                          "<drools:resource source=\"classpath:changesets/change-set-2.xml\" type=\"CHANGE_SET\" />"+
+			                          "<drools:kbase>"+
+			                          "<drools:ksession id=\"ksession1\" type=\"stateful\"  kbase=\"kbase1\" node=\"node1\"/>"+
+			                          "<drools:ksession id=\"ksession2\" type=\"stateless\" kbase=\"kbase2\" node=\"node1\"/>"+
+			                          "</beans>";
 
+			String droolsSpringCtxtErr2 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+			  "<beans xmlns=\"http://www.springframework.org/schema/beans\"\n"+
+            "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"+
+            "xmlns:context=\"http://www.springframework.org/schema/context\"\n"+
+            "xmlns:drools=\"http://drools.org/schema/drools-spring\"\n"+
+            "xsi:schemaLocation=\"http://www.springframework.org/schema/beans\n"+
+            " http://www.springframework.org/schema/beans/spring-beans-2.5.xsd\n"+
+            " http://www.springframework.org/schema/context\n"+
+            " http://www.springframework.org/schema/context/spring-context-2.5.xsd\n"+
+            " http://drools.org/schema/drools-spring\n"+
+            " http://drools.org/schema/drools-spring.xsd\""+ 
+            " default-autowire=\"byName\">" +
+            "<pepe:abc id=\"connection1\" type=\"local\" />"+
+            "<drools:execution-node id=\"node1\" connection=\"connection1\" />"+
+            "<drools:kbase id=\"kbase1\" node=\"node1\">"+
+            "<drools:resource source=\"classpath:changesets/change-set-1.xml\" type=\"CHANGE_SET\" />"+
+            "<drools:model source=\"classpath:model/person.xsd\" />"+
+            "</drools:kbase>"+
+            "<drools:kbase id=\"kbase2\" node=\"node1\">"+
+            "<drools:resource source=\"classpath:changesets/change-set-2.xml\" type=\"CHANGE_SET\" />"+
+            "<drools:kbase>"+
+            "<drools:ksession id=\"ksession1\" type=\"stateful\"  kbase=\"kbase1\" node=\"node1\"/>"+
+            "<drools:ksession id=\"ksession2\" type=\"stateless\" kbase=\"kbase2\" node=\"node1\"/>"+
+            "</beans>";
 			
 		
 		/*Validate Common Spring Context*/
@@ -59,6 +109,12 @@ public class SpringContextValidatorTest {
 		/*Validate Drools Spring Integration*/
 		validator.setContent(droolsSpringCtxt);
 		assertEquals(validator.validate(),"");
+		/*Validate Drools Spring Integration Sixntax Error*/
+		validator.setContent(droolsSpringCtxtErr1);
+		assertNotSame(validator.validate(),"");
+		/*Validate Drools Spring Integration Semantic Error*/
+		validator.setContent(droolsSpringCtxtErr2);
+		assertNotSame(validator.validate(),"");
 	}
 	
 	    
