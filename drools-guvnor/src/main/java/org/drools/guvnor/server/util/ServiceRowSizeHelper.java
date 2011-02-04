b95f9980345cc751d64062c1a08f391b481c1ffd
Jari Timonen <jari.timonen@zeniitti.net>
2011-02-04 19:53:33 +0200

    GUVNOR-1133: Added javadocs

--- drools-guvnor/src/main/java/org/drools/guvnor/server/util/ServiceRowSizeHelper.java
+++ drools-guvnor/src/main/java/org/drools/guvnor/server/util/ServiceRowSizeHelper.java
@@ -1,9 +1,29 @@
+/**
+ * Copyright 2011 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.drools.guvnor.server.util;
 
 import org.drools.guvnor.client.rpc.AbstractPageRow;
 import org.drools.guvnor.client.rpc.PageRequest;
 import org.drools.guvnor.client.rpc.PageResponse;
 
+/**
+ * 
+ * @author Jari Timonen
+ *
+ */
 public class ServiceRowSizeHelper {
     // The total Record Count returned from AssetItemIterator.getSize() can be
     // -1 which is not very helpful. We can however derive the total row count
