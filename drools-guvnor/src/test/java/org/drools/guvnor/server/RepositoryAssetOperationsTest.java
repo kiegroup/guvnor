f798eefbce1c757c33042b52203e5ce85549c6c5
Jari Timonen <jari.timonen@zeniitti.net>
2011-02-06 10:54:19 +0200

    GUVNOR-1133: Added test for RepositoryAssetOperations, refactored AssetPageRowPopulatorTest

---
+++ drools-guvnor/src/test/java/org/drools/guvnor/server/RepositoryAssetOperationsTest.java
@@ -0,0 +1,116 @@
+/**
+ * Copyright 2011 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.drools.guvnor.server;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.drools.guvnor.client.rpc.TableDataResult;
+import org.drools.guvnor.client.rpc.TableDataRow;
+import org.drools.repository.AssetHistoryIterator;
+import org.drools.repository.AssetItem;
+import org.drools.repository.RulesRepository;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.google.gwt.user.client.rpc.SerializationException;
+
+public class RepositoryAssetOperationsTest {
+
+    @Test
+    public void testRenameAsset() {
+        RulesRepository rulesRepository = Mockito.mock( RulesRepository.class );
+        RepositoryAssetOperations repositoryAssetOperations = new RepositoryAssetOperations();
+        repositoryAssetOperations.setRulesRepository( rulesRepository );
+        when( rulesRepository.renameAsset( "uuid",
+                                           "newname" ) ).thenReturn( "uuid" );
+        assertEquals( repositoryAssetOperations.renameAsset( "uuid",
+                                                             "newname" ),
+                      "uuid" );
+    }
+
+    @Test
+    public void thenLoadAssetHistoryIsNull() throws SerializationException {
+        RulesRepository rulesRepository = mock( RulesRepository.class );
+        RepositoryAssetOperations repositoryAssetOperations = new RepositoryAssetOperations();
+        repositoryAssetOperations.setRulesRepository( rulesRepository );
+
+        AssetItem assetItem = mock( AssetItem.class );
+
+        AssetHistoryIterator assetHistoryIterator = mock( AssetHistoryIterator.class );
+        when( assetItem.getHistory() ).thenReturn( assetHistoryIterator );
+
+        assertNull( repositoryAssetOperations.loadAssetHistory( assetItem ) );
+    }
+
+    @Test
+    public void thenLoadAssetHistoryAndHistoryDoesNotExistsAndNullIsReturned() throws SerializationException {
+        RulesRepository rulesRepository = mock( RulesRepository.class );
+        RepositoryAssetOperations repositoryAssetOperations = new RepositoryAssetOperations();
+        repositoryAssetOperations.setRulesRepository( rulesRepository );
+
+        AssetItem assetItem = mock( AssetItem.class );
+        when( assetItem.getVersionNumber() ).thenReturn( 1324567L );
+
+        AssetHistoryIterator assetHistoryIterator = mock( AssetHistoryIterator.class );
+        when( assetItem.getHistory() ).thenReturn( assetHistoryIterator );
+        when( assetHistoryIterator.hasNext() ).thenReturn( true,
+                                                               false );
+        AssetItem historicalAssetItem = mock( AssetItem.class );
+        when( assetHistoryIterator.next() ).thenReturn( historicalAssetItem );
+        when( historicalAssetItem.getVersionNumber() ).thenReturn( 1324567L );
+
+        assertNull( repositoryAssetOperations.loadAssetHistory( assetItem ) );
+
+    }
+
+    @Test
+    public void thenLoadAssetHistoryAndHistoryExists() throws SerializationException {
+        RulesRepository rulesRepository = mock( RulesRepository.class );
+        RepositoryAssetOperations repositoryAssetOperations = new RepositoryAssetOperations();
+        repositoryAssetOperations.setRulesRepository( rulesRepository );
+
+        AssetItem assetItem = mock( AssetItem.class );
+        when( assetItem.getVersionNumber() ).thenReturn( 1324567L );
+
+        AssetHistoryIterator assetHistoryIterator = mock( AssetHistoryIterator.class );
+        when( assetItem.getHistory() ).thenReturn( assetHistoryIterator );
+        when( assetHistoryIterator.hasNext() ).thenReturn( true,
+                                                               false );
+
+        AssetItem historicalAssetItem = mock( AssetItem.class );
+        when( assetHistoryIterator.next() ).thenReturn( historicalAssetItem );
+        when( historicalAssetItem.getVersionNumber() ).thenReturn( 123456L );
+
+        Calendar calendar = GregorianCalendar.getInstance();
+        when( historicalAssetItem.getLastModified() ).thenReturn( calendar );
+
+        TableDataResult tableDataResult = repositoryAssetOperations.loadAssetHistory( assetItem );
+        assertNotNull( tableDataResult );
+        TableDataRow[] tableDataRow = tableDataResult.data;
+        assertNotNull( tableDataRow );
+        assertEquals( tableDataRow.length,
+                          1 );
+
+    }
+
+}
