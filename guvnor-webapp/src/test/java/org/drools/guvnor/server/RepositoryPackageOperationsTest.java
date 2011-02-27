9fe7590e11f68aa462d7d0898087bb6a29ab5b91
Jari Timonen <jari.timonen@zeniitti.net>
2011-02-27 11:29:07 +0200

    GUVNOR-1208: removed unused import

--- guvnor-webapp/src/test/java/org/drools/guvnor/server/RepositoryPackageOperationsTest.java
+++ guvnor-webapp/src/test/java/org/drools/guvnor/server/RepositoryPackageOperationsTest.java
@@ -1,9 +1,9 @@
 package org.drools.guvnor.server;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -15,7 +15,6 @@ import org.drools.guvnor.client.rpc.PackageConfigData;
 import org.drools.repository.PackageItem;
 import org.drools.repository.RulesRepository;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 public class RepositoryPackageOperationsTest {
 
@@ -34,52 +33,52 @@ public class RepositoryPackageOperationsTest {
         assertEquals( c2,
                       ls.get( 1 ) );
     }
-    
+
     @Test
-    public void testLoadGlobalPackageAndDependenciesAreNotFetched(){
+    public void testLoadGlobalPackageAndDependenciesAreNotFetched() {
         RulesRepository rulesRepository = mock( RulesRepository.class );
         RepositoryPackageOperations packageOperations = new RepositoryPackageOperations();
         packageOperations.setRulesRepository( rulesRepository );
-        PackageItem packageItem = mock(PackageItem.class);
+        PackageItem packageItem = mock( PackageItem.class );
         when( rulesRepository.loadGlobalArea() ).thenReturn( packageItem );
-        when( packageItem.getLastModified()).thenReturn( GregorianCalendar.getInstance() );
-        when( packageItem.getCreatedDate()).thenReturn( GregorianCalendar.getInstance() );
-        when(packageItem.getDependencies()).thenReturn( new String[]{"dependency"} );
-        assertNull(packageOperations.loadGlobalPackage().dependencies);
-        
-    }
-    
-    @Test
-    public void testLoadGlobalPackageAndIsSnapshot(){
-        RulesRepository rulesRepository = mock( RulesRepository.class );
-        RepositoryPackageOperations packageOperations = new RepositoryPackageOperations();
-        packageOperations.setRulesRepository( rulesRepository );
-        PackageItem packageItem = mock(PackageItem.class);
-        when( rulesRepository.loadGlobalArea() ).thenReturn( packageItem );
-        when( packageItem.getLastModified()).thenReturn( GregorianCalendar.getInstance() );
-        when( packageItem.getCreatedDate()).thenReturn( GregorianCalendar.getInstance() );
-        when(packageItem.isSnapshot()).thenReturn( true );
-        when(packageItem.getSnapshotName()).thenReturn( "snapshotName123" );
-        assertTrue(packageOperations.loadGlobalPackage().isSnapshot);
-        assertEquals(packageOperations.loadGlobalPackage().snapshotName,"snapshotName123" );
-        
-    }
-    
-    @Test
-    public void testLoadGlobalPackageAndIsNotSnapshot(){
-        RulesRepository rulesRepository = mock( RulesRepository.class );
-        RepositoryPackageOperations packageOperations = new RepositoryPackageOperations();
-        packageOperations.setRulesRepository( rulesRepository );
-        PackageItem packageItem = mock(PackageItem.class);
-        when( rulesRepository.loadGlobalArea() ).thenReturn( packageItem );
-        when( packageItem.getLastModified()).thenReturn( GregorianCalendar.getInstance() );
-        when( packageItem.getCreatedDate()).thenReturn( GregorianCalendar.getInstance() );
-        when(packageItem.isSnapshot()).thenReturn( false );
-        when(packageItem.getSnapshotName()).thenReturn( "snapshotName123" );
-        assertFalse(packageOperations.loadGlobalPackage().isSnapshot);
-        assertNull(packageOperations.loadGlobalPackage().snapshotName);
-        
+        when( packageItem.getLastModified() ).thenReturn( GregorianCalendar.getInstance() );
+        when( packageItem.getCreatedDate() ).thenReturn( GregorianCalendar.getInstance() );
+        when( packageItem.getDependencies() ).thenReturn( new String[]{"dependency"} );
+        assertNull( packageOperations.loadGlobalPackage().dependencies );
+
     }
 
+    @Test
+    public void testLoadGlobalPackageAndIsSnapshot() {
+        RulesRepository rulesRepository = mock( RulesRepository.class );
+        RepositoryPackageOperations packageOperations = new RepositoryPackageOperations();
+        packageOperations.setRulesRepository( rulesRepository );
+        PackageItem packageItem = mock( PackageItem.class );
+        when( rulesRepository.loadGlobalArea() ).thenReturn( packageItem );
+        when( packageItem.getLastModified() ).thenReturn( GregorianCalendar.getInstance() );
+        when( packageItem.getCreatedDate() ).thenReturn( GregorianCalendar.getInstance() );
+        when( packageItem.isSnapshot() ).thenReturn( true );
+        when( packageItem.getSnapshotName() ).thenReturn( "snapshotName123" );
+        assertTrue( packageOperations.loadGlobalPackage().isSnapshot );
+        assertEquals( packageOperations.loadGlobalPackage().snapshotName,
+                      "snapshotName123" );
+
+    }
+
+    @Test
+    public void testLoadGlobalPackageAndIsNotSnapshot() {
+        RulesRepository rulesRepository = mock( RulesRepository.class );
+        RepositoryPackageOperations packageOperations = new RepositoryPackageOperations();
+        packageOperations.setRulesRepository( rulesRepository );
+        PackageItem packageItem = mock( PackageItem.class );
+        when( rulesRepository.loadGlobalArea() ).thenReturn( packageItem );
+        when( packageItem.getLastModified() ).thenReturn( GregorianCalendar.getInstance() );
+        when( packageItem.getCreatedDate() ).thenReturn( GregorianCalendar.getInstance() );
+        when( packageItem.isSnapshot() ).thenReturn( false );
+        when( packageItem.getSnapshotName() ).thenReturn( "snapshotName123" );
+        assertFalse( packageOperations.loadGlobalPackage().isSnapshot );
+        assertNull( packageOperations.loadGlobalPackage().snapshotName );
+
+    }
 
 }
