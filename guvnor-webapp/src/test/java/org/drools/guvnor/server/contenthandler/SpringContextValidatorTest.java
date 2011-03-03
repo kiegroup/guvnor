5612ff8dc89589b2efa6e788a006e6af11f1e3e2
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2011-03-03 15:00:19 +0100

    identation

--- guvnor-webapp/src/test/java/org/drools/guvnor/server/contenthandler/SpringContextValidatorTest.java
+++ guvnor-webapp/src/test/java/org/drools/guvnor/server/contenthandler/SpringContextValidatorTest.java
@@ -9,29 +9,28 @@ import org.junit.Test;
 
 public class SpringContextValidatorTest {
 
+    @Test
+    public void testValidContext(){
+        SpringContextValidator validator = new SpringContextValidator();
+        InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("org/drools/guvnor/server/contenthandler/valid-spring-context.xml");
+        validator.setContent(resourceAsStream);
+        assertEquals("",validator.validate());
+    }
 
-        @Test
-        public void testValidContext(){
-            SpringContextValidator validator = new SpringContextValidator();
-            InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("org/drools/guvnor/server/contenthandler/valid-spring-context.xml");
-            validator.setContent(resourceAsStream);
-            assertEquals("",validator.validate());
-        }
-        
-        @Test
-        public void testInvalidContext(){
-            SpringContextValidator validator = new SpringContextValidator();
-            InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("org/drools/guvnor/server/contenthandler/invalid-spring-context.xml");
-            validator.setContent(resourceAsStream);
-            assertFalse(validator.validate().isEmpty());
-        }
-        
-        @Test
-        public void testMalformedContext(){
-            SpringContextValidator validator = new SpringContextValidator();
-            InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("org/drools/guvnor/server/contenthandler/malformed-spring-context.xml");
-            validator.setContent(resourceAsStream);
-            assertFalse(validator.validate().isEmpty());
-        }
+    @Test
+    public void testInvalidContext(){
+        SpringContextValidator validator = new SpringContextValidator();
+        InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("org/drools/guvnor/server/contenthandler/invalid-spring-context.xml");
+        validator.setContent(resourceAsStream);
+        assertFalse(validator.validate().isEmpty());
+    }
+
+    @Test
+    public void testMalformedContext(){
+        SpringContextValidator validator = new SpringContextValidator();
+        InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("org/drools/guvnor/server/contenthandler/malformed-spring-context.xml");
+        validator.setContent(resourceAsStream);
+        assertFalse(validator.validate().isEmpty());
+    }
         
 }
