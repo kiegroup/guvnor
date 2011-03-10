d4c4eab95a0a370fbf2b7c54fa90ccba4d83903b
Toni Rikkola <toni@rikkola.net>
2011-03-10 15:36:29 +0200

    GUVNOR-1125 : Guvnor Perspectives -More license headers -Bug fix in IFramePlaces

--- guvnor-webapp/src/main/java/org/drools/guvnor/client/explorer/IFramePerspectiveViewImpl.java
+++ guvnor-webapp/src/main/java/org/drools/guvnor/client/explorer/IFramePerspectiveViewImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 JBoss Inc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 package org.drools.guvnor.client.explorer;
 
 import com.google.gwt.user.client.ui.Composite;
@@ -5,17 +21,29 @@ import com.google.gwt.user.client.ui.Frame;
 
 public class IFramePerspectiveViewImpl extends Composite implements IFramePerspectiveView {
 
-    private Frame frame = new Frame("http://localhost:8080/gwt-console");
+    private Frame frame = new Frame();
+
+    private Presenter presenter;
+
+    private String name;
 
     public IFramePerspectiveViewImpl() {
         initWidget(frame);
     }
 
     public String getName() {
-        return "CCCCCC";
+        return "Drools Manual";
     }
 
     public void setPresenter(Presenter presenter) {
-        //TODO: Generated code -Rikkola-
+        this.presenter = presenter;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUrl(String url) {
+        frame.setUrl(url);
     }
 }
