bb83b01c99cdd598edadf3642505d3c7db6584bb
Jari Timonen <jari.timonen@zeniitti.net>
2011-05-01 10:26:13 +0300

    GUVNOR-1133 removed unused

--- guvnor-webapp/src/main/java/org/drools/guvnor/server/files/AssetZipper.java
+++ guvnor-webapp/src/main/java/org/drools/guvnor/server/files/AssetZipper.java
@@ -1,20 +1,13 @@
 package org.drools.guvnor.server.files;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
-
 
 import org.drools.repository.AssetItem;
 import org.drools.repository.PackageItem;
@@ -26,57 +19,57 @@ import org.drools.repository.PackageItem;
 public class AssetZipper {
 
     LinkedList<AssetItem> assets;
-    PackageItem pkg;
+    PackageItem           pkg;
 
-    public AssetZipper(LinkedList<AssetItem> assets, PackageItem pkg){
+    public AssetZipper(LinkedList<AssetItem> assets,
+                       PackageItem pkg) {
         this.assets = assets;
         this.pkg = pkg;
     }
 
-    public InputStream zipAssets(){
+    public InputStream zipAssets() {
 
         AssetItem zipFileElement = null;
         BufferedInputStream inputZip = null;
         Iterator<AssetItem> it = assets.iterator();
         byte[] data = new byte[1000];
-        InputStream modelStream = null;
         int count = 0;
 
-            try{
+        try {
 
-                if (assets.size() > 1){
-                        ByteArrayOutputStream outputBytes = new ByteArrayOutputStream();
-                        ZipOutputStream outputZip = new ZipOutputStream(outputBytes);
+            if ( assets.size() > 1 ) {
+                ByteArrayOutputStream outputBytes = new ByteArrayOutputStream();
+                ZipOutputStream outputZip = new ZipOutputStream( outputBytes );
 
-                        while(it.hasNext()){
+                while ( it.hasNext() ) {
 
-                            zipFileElement = it.next();
-                            inputZip = new BufferedInputStream(zipFileElement.getBinaryContentAttachment());
-                            outputZip.putNextEntry(new ZipEntry(zipFileElement.getName()+ "." + zipFileElement.getFormat()));
+                    zipFileElement = it.next();
+                    inputZip = new BufferedInputStream( zipFileElement.getBinaryContentAttachment() );
+                    outputZip.putNextEntry( new ZipEntry( zipFileElement.getName() + "." + zipFileElement.getFormat() ) );
 
-                                while((count = inputZip.read(data,0,1000)) != -1)
-                                {
-                                    outputZip.write(data, 0, count);
-                                }
+                    while ( (count = inputZip.read( data,
+                                                    0,
+                                                    1000 )) != -1 ) {
+                        outputZip.write( data,
+                                         0,
+                                         count );
+                    }
 
-                                 outputZip.flush();
-                                 inputZip.close();
-                        }
-                        outputZip.close();
-
-                        return new ByteArrayInputStream(outputBytes.toByteArray());
-                }else{
-
-                    return it.next().getBinaryContentAttachment();
+                    outputZip.flush();
+                    inputZip.close();
                 }
-            }catch(Exception ex){
+                outputZip.close();
 
+                return new ByteArrayInputStream( outputBytes.toByteArray() );
+            } else {
 
+                return it.next().getBinaryContentAttachment();
             }
+        } catch ( Exception ex ) {
 
-            return null;
+        }
 
-
+        return null;
 
     }
 
