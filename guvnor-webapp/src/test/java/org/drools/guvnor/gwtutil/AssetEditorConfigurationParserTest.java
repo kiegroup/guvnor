fbb011120adc729e799120b4bcce2b089cb26b74
Michael Anstis <michael.anstis@gmail.com>
2011-07-13 09:29:35 +0100

    Fixed failing test following GUVNOR-1522: POJO Model asset type is not listed under the "Model" node

--- guvnor-webapp/src/test/java/org/drools/guvnor/gwtutil/AssetEditorConfigurationParserTest.java
+++ guvnor-webapp/src/test/java/org/drools/guvnor/gwtutil/AssetEditorConfigurationParserTest.java
@@ -30,7 +30,7 @@ public class AssetEditorConfigurationParserTest {
 		AssetEditorConfigurationParser a = new AssetEditorConfigurationParser();
 		List<AssetEditorConfiguration> configList = a.readConfig();
 
-        assertEquals(22, configList.size());
+        assertEquals(23, configList.size());
         boolean foundPropertiesWidgetForXML = false;
         boolean foundPropertiesWidgetForDocument = false;
         for(AssetEditorConfiguration config:configList) {
