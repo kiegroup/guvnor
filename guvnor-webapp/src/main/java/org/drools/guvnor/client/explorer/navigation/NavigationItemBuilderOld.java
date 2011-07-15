b744006823641b84164cdc23b0a635da92b1697b
Toni Rikkola <toni@rikkola.net>
2011-07-15 14:44:44 +0300

    cleaning the codes

--- guvnor-webapp/src/main/java/org/drools/guvnor/client/explorer/navigation/NavigationItemBuilderOld.java
+++ guvnor-webapp/src/main/java/org/drools/guvnor/client/explorer/navigation/NavigationItemBuilderOld.java
@@ -19,15 +19,14 @@ package org.drools.guvnor.client.explorer.navigation;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.logical.shared.SelectionHandler;
 import com.google.gwt.user.client.ui.*;
-import org.drools.guvnor.client.explorer.TabContainer;
-import org.drools.guvnor.client.explorer.TabManager;
-import org.drools.guvnor.client.ruleeditor.MultiViewRow;
-import org.drools.guvnor.client.ruleeditor.NewAssetWizard;
-import org.drools.guvnor.client.rulelist.OpenItemCommand;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * You should use refactor any navigation items builders to use NavigationBuilder
+ */
+@Deprecated
 public abstract class NavigationItemBuilderOld extends Composite
         implements
         SelectionHandler<TreeItem>, NavigationItem {
@@ -49,18 +48,6 @@ public abstract class NavigationItemBuilderOld extends Composite
         }
 
         dockLayoutPanel.add( new ScrollPanel( mainTree ) );
-
-        /*
-
-        TODO: wrap with DockLayoutPanel if has a menu, if not, just wrap with a ScrollPanel -Rikkola-
-        TODO: check the permissions to show menus in the AbstractTree implementations -Rikkola-
-        TODO: check the permissions to show abstract tree implementations in NavigationPanel, but keep the logic for this in the AT impl -Rikkola-
-        TODO: This should be an interface -Rikkola-
-        TODO: Deprecate TabOpener and create places -Rikkola-
-
-
-         */
-
 
         initWidget( dockLayoutPanel );
     }
