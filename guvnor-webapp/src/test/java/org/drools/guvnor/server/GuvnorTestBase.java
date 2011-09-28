0040b383612ac76bb19a8b42c7db2283a6758a76
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2011-09-28 17:32:22 +0200

    version change

--- guvnor-webapp/src/test/java/org/drools/guvnor/server/GuvnorTestBase.java
+++ guvnor-webapp/src/test/java/org/drools/guvnor/server/GuvnorTestBase.java
@@ -44,7 +44,7 @@ public abstract class GuvnorTestBase {
     @Deployment
     public static WebArchive createDeployment() {
         // TODO FIXME do not hardcode the version number
-        File explodedWarFile = new File("target/guvnor-webapp-5.3.0-SNAPSHOT/");
+        File explodedWarFile = new File("target/guvnor-webapp-5.4.0-SNAPSHOT/");
         if (!explodedWarFile.exists()) {
             throw new IllegalStateException("The exploded war file (" + explodedWarFile
                     + ") should exist, run \"mvn package\" first.");
