6d0f026f2f53b5adc89288b034434348503815b0
esteban-aliverti <esteban.aliverti@gmail.com>
2011-12-09 12:31:51 +0100

    - Removed unused import

--- guvnor-webapp-drools/src/main/java/org/drools/guvnor/client/asseteditor/drools/DefaultOryxMultiViewEditorMenuBarCreator.java
+++ guvnor-webapp-drools/src/main/java/org/drools/guvnor/client/asseteditor/drools/DefaultOryxMultiViewEditorMenuBarCreator.java
@@ -7,7 +7,6 @@ import org.drools.guvnor.client.messages.Constants;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.user.client.Command;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.MenuBar;
 import org.drools.guvnor.client.asseteditor.AfterAssetEditorCheckInEvent;
 
