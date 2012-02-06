c56f443e49dca72d4f6944590c92c6bf2eb35245
Michael Anstis <michael.anstis@gmail.com>
2012-02-06 21:29:32 +0000

    GUVNOR-13: Guided decision table: Import XLS Decision Tables into guided editor. Disable until complete.

--- guvnor-webapp-drools/src/main/java/org/drools/guvnor/client/decisiontable/DecisionTableXLSWidget.java
+++ guvnor-webapp-drools/src/main/java/org/drools/guvnor/client/decisiontable/DecisionTableXLSWidget.java
@@ -51,7 +51,7 @@ public class DecisionTableXLSWidget extends AssetAttachmentFileWidget {
 
         //Set-up supplementary widgets
         if ( !asset.isReadonly() ) {
-            super.addSupplementaryWidget( makeConvertToGuidedDecisionTableWidget( asset ) );
+            //TODO {manstis} Uncomment - super.addSupplementaryWidget( makeConvertToGuidedDecisionTableWidget( asset ) );
         }
         super.addSupplementaryWidget( makeDescriptionWidget() );
     }
