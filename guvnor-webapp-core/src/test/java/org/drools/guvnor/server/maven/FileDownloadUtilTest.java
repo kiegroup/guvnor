46d59799524a50f8a9b10e5d52518f1420574fc7
Michael Anstis <michael.anstis@gmail.com>
2012-02-23 10:17:00 +0000

    Fix tests (to GWT2.3 level)

--- guvnor-webapp-core/src/test/java/org/drools/guvnor/server/maven/FileDownloadUtilTest.java
+++ guvnor-webapp-core/src/test/java/org/drools/guvnor/server/maven/FileDownloadUtilTest.java
@@ -16,6 +16,14 @@
 
 package org.drools.guvnor.server.maven;
 
+import static org.apache.commons.codec.digest.DigestUtils.md5Hex;
+import static org.apache.commons.codec.digest.DigestUtils.shaHex;
+import static org.drools.guvnor.server.maven.cache.FileDownloadUtil.downloadFile;
+import static org.drools.guvnor.server.maven.cache.GuvnorArtifactCacheSupport.getURLtoLocalUserMavenRepo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -26,60 +34,67 @@ import java.net.UnknownHostException;
 import org.drools.guvnor.client.rpc.MavenArtifact;
 import org.junit.Test;
 
-import static org.apache.commons.codec.digest.DigestUtils.*;
-import static org.drools.guvnor.server.maven.cache.FileDownloadUtil.*;
-import static org.drools.guvnor.server.maven.cache.GuvnorArtifactCacheSupport.*;
-import static org.junit.Assert.*;
-
 public class FileDownloadUtilTest {
 
     @Test
     public void testDownloadRemoteRepository() throws IOException {
         final String jbossReleaseRepo = "http://repository.jboss.org/nexus/content/repositories/releases/";
-        final MavenArtifact artifact = new MavenArtifact("org.drools:knowledge-api:jar:5.3.1.Final:compile");
+        final MavenArtifact artifact = new MavenArtifact( "org.drools:knowledge-api:jar:5.3.1.Final:compile" );
 
-        final HttpURLConnection connection = (HttpURLConnection) new URL(artifact.toURL(jbossReleaseRepo)).openConnection();
-        connection.setRequestMethod("HEAD");
+        final HttpURLConnection connection = (HttpURLConnection) new URL( artifact.toURL( jbossReleaseRepo ) ).openConnection();
+        connection.setRequestMethod( "HEAD" );
         try {
             final int responseCode = connection.getResponseCode();
-            if (responseCode != 200) {
+            if ( responseCode != 200 ) {
                 //won't fail if you can't get access to external resource
                 return;
             }
-        } catch (UnknownHostException ex) {
+        } catch ( UnknownHostException ex ) {
             //won't fail if you can't reach jboss repo
             return;
         }
 
-        final String finalFileName = System.getProperty("java.io.tmpdir") + "/" + artifact.toFileName();
-        final File result = downloadFile(artifact.toURL(jbossReleaseRepo), finalFileName);
+        final String finalFileName = System.getProperty( "java.io.tmpdir" ) + "/" + artifact.toFileName();
+        final File result = downloadFile( artifact.toURL( jbossReleaseRepo ),
+                                          finalFileName );
 
-        assertNotNull(result);
-        assertEquals("47c645f0f605790b5d505c7d2f27b745", md5Hex(new FileInputStream(result)));
+        assertNotNull( result );
+        assertEquals( "47c645f0f605790b5d505c7d2f27b745",
+                      md5Hex( getBytes( result ) ) );
     }
 
     @Test
     public void testLocalFile() throws IOException {
         final String localMavenRepo = getURLtoLocalUserMavenRepo();
-        final MavenArtifact artifact = new MavenArtifact("org.antlr:antlr-runtime:jar:3.3:compile");
+        final MavenArtifact artifact = new MavenArtifact( "org.antlr:antlr-runtime:jar:3.3:compile" );
 
-        final String finalFileName = System.getProperty("java.io.tmpdir") + "/" + artifact.toFileName();
+        final String finalFileName = System.getProperty( "java.io.tmpdir" ) + "/" + artifact.toFileName();
 
-        final File result = downloadFile(artifact.toURL(localMavenRepo), finalFileName);
+        final File result = downloadFile( artifact.toURL( localMavenRepo ),
+                                          finalFileName );
 
-        assertNotNull(result);
-        assertEquals("ccd65b08cbc9b7e90b9facd4d125a133c6f87228", shaHex(new FileInputStream(result)));
+        assertNotNull( result );
+        assertEquals( "ccd65b08cbc9b7e90b9facd4d125a133c6f87228",
+                      shaHex( getBytes( result ) ) );
     }
 
     @Test
     public void testNonExistentFile() throws IOException {
         final String localMavenRepo = getURLtoLocalUserMavenRepo();
-        final MavenArtifact artifact = new MavenArtifact("org.antlr:antlr-some:jar:3.3:compile");
+        final MavenArtifact artifact = new MavenArtifact( "org.antlr:antlr-some:jar:3.3:compile" );
 
-        final String finalFileName = System.getProperty("java.io.tmpdir") + "/" + artifact.toFileName();
+        final String finalFileName = System.getProperty( "java.io.tmpdir" ) + "/" + artifact.toFileName();
 
-        final File result = downloadFile(artifact.toURL(localMavenRepo), finalFileName);
+        final File result = downloadFile( artifact.toURL( localMavenRepo ),
+                                          finalFileName );
 
-        assertNull(result);
+        assertNull( result );
+    }
+
+    private byte[] getBytes(File file) throws IOException {
+        byte[] bytes = new byte[(int) file.length()];
+        FileInputStream fileInputStream = new FileInputStream( file );
+        fileInputStream.read( bytes );
+        return bytes;
     }
 }
