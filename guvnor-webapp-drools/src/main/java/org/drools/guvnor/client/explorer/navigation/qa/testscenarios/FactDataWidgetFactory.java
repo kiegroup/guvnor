842c417a9071d0ad3f1cd11358a0aa4c7fba8e89
Toni Rikkola <toni@rikkola.net>
2012-03-02 13:20:42 +0200

    GUVNOR-1174 : Test Scenarios: Is not possible set facts sub-fields

--- guvnor-webapp-drools/src/main/java/org/drools/guvnor/client/explorer/navigation/qa/testscenarios/FactDataWidgetFactory.java
+++ guvnor-webapp-drools/src/main/java/org/drools/guvnor/client/explorer/navigation/qa/testscenarios/FactDataWidgetFactory.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.drools.guvnor.client.explorer.navigation.qa.testscenarios;
 
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -12,7 +28,7 @@ import org.drools.guvnor.client.common.DirtyableFlexTable;
 import org.drools.guvnor.client.common.ImageButton;
 import org.drools.guvnor.client.common.SmallLabel;
 import org.drools.guvnor.client.messages.Constants;
-import org.drools.guvnor.client.resources.Images;
+import org.drools.guvnor.client.resources.DroolsGuvnorImages;
 import org.drools.ide.common.client.modeldriven.SuggestionCompletionEngine;
 import org.drools.ide.common.client.modeldriven.testing.*;
 import org.drools.ide.common.client.modeldriven.testing.FactAssignmentField;
@@ -164,7 +180,7 @@ public class FactDataWidgetFactory {
     class DeleteFactColumnButton extends ImageButton {
 
         public DeleteFactColumnButton(final FactData factData) {
-            super(Images.INSTANCE.deleteItemSmall(),
+            super(DroolsGuvnorImages.INSTANCE.deleteItemSmall(),
                     Constants.INSTANCE.RemoveTheColumnForScenario(factData.getName()));
 
             addClickHandler(new ClickHandler() {
@@ -186,7 +202,7 @@ public class FactDataWidgetFactory {
     class DeleteFieldRowButton extends ImageButton {
         public DeleteFieldRowButton(final String factName,
                                     final String fieldName) {
-            super(Images.INSTANCE.deleteItemSmall(),
+            super(DroolsGuvnorImages.INSTANCE.deleteItemSmall(),
                     Constants.INSTANCE.RemoveThisRow());
 
             addClickHandler(new ClickHandler() {
--- guvnor-webapp-drools/src/main/java/org/drools/guvnor/client/explorer/navigation/qa/testscenarios/TypeChoiceFormPopup.java
+++ guvnor-webapp-drools/src/main/java/org/drools/guvnor/client/explorer/navigation/qa/testscenarios/TypeChoiceFormPopup.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.drools.guvnor.client.explorer.navigation.qa.testscenarios;
 
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -11,7 +27,7 @@ import org.drools.guvnor.client.common.FormStylePopup;
 import org.drools.guvnor.client.common.InfoPopup;
 import org.drools.guvnor.client.common.SmallLabel;
 import org.drools.guvnor.client.messages.Constants;
-import org.drools.guvnor.client.resources.Images;
+import org.drools.guvnor.client.resources.DroolsGuvnorImages;
 import org.drools.ide.common.client.modeldriven.testing.FieldData;
 
 public class TypeChoiceFormPopup
@@ -20,7 +36,7 @@ public class TypeChoiceFormPopup
 
 
     public TypeChoiceFormPopup(FieldDataConstraintHelper helper) {
-        super(Images.INSTANCE.newexWiz(),
+        super(DroolsGuvnorImages.INSTANCE.newexWiz(),
                 Constants.INSTANCE.FieldValue());
 
 
