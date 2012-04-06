95cce4873b3bcfbae2de1f8ed7ec72480d2db58d
Michael Anstis <michael.anstis@gmail.com>
2012-04-06 09:19:02 +0100

    Fix target folder name for ShrinkWrap.

--- guvnor-webapp-drools/src/test/java/org/drools/guvnor/server/test/GuvnorIntegrationTest.java
+++ guvnor-webapp-drools/src/test/java/org/drools/guvnor/server/test/GuvnorIntegrationTest.java
@@ -54,7 +54,7 @@ public abstract class GuvnorIntegrationTest {
                 .as(WebArchive.class);
         addTestDependencies(webArchive);
 
-        File explodedWarFile = new File("target/guvnor-webapp-drools-5.4.0-SNAPSHOT");
+        File explodedWarFile = new File("target/guvnor-webapp-drools-5.5.0-SNAPSHOT");
         if (!explodedWarFile.exists()) {
             throw new IllegalStateException("The exploded war file (" + explodedWarFile
                     + ") should exist, run \"mvn package\" first.");
