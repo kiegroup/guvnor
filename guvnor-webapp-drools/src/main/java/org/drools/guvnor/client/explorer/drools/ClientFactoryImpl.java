786d48cac87034c4c7f870c7450abc20feac92fa
Toni Rikkola <toni@rikkola.net>
2012-09-21 13:49:31 +0300

    got Uberfire working again

--- guvnor-webapp-drools/src/main/java/org/drools/guvnor/client/explorer/drools/ClientFactoryImpl.java
+++ guvnor-webapp-drools/src/main/java/org/drools/guvnor/client/explorer/drools/ClientFactoryImpl.java
@@ -38,6 +38,9 @@ public class ClientFactoryImpl extends AbstractClientFactoryImpl {
     @Inject
     PlaceManager placeManager;
 
+//    @Inject
+    PlaceManager placeManager;
+
     @Inject
     PlaceManager placeManager;
 
