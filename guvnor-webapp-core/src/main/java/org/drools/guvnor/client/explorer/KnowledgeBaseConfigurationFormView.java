a54952527d3b10ac6e4accc5c5f846fa602691d1
Toni Rikkola <toni@rikkola.net>
2012-11-12 12:54:32 +0200

    GUVNOR-1969 : Module Editor for KBase configurations

---
+++ guvnor-webapp-core/src/main/java/org/drools/guvnor/client/explorer/KnowledgeBaseConfigurationFormView.java
@@ -0,0 +1,11 @@
+package org.drools.guvnor.client.explorer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rikkola
+ * Date: 11/9/12
+ * Time: 3:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class KnowledgeBaseConfigurationFormView {
+}
---
+++ guvnor-webapp-core/src/main/java/org/drools/guvnor/client/explorer/ProjectModelScreenViewImpl.java
@@ -0,0 +1,11 @@
+package org.drools.guvnor.client.explorer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rikkola
+ * Date: 11/9/12
+ * Time: 12:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProjectModelScreenViewImpl implements ProjectModelScreenView {
+}
