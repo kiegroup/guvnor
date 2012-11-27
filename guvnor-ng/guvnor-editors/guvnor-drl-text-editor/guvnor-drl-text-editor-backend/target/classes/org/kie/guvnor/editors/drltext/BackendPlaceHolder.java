0b8c223bf16a182209eff92d5808f2459ff4f952
Michael Anstis <michael.anstis@gmail.com>
2012-11-27 22:32:46 +0000

    Remove target folders added before .gitignore files were added

--- guvnor-ng/guvnor-editors/guvnor-drl-text-editor/guvnor-drl-text-editor-backend/target/classes/org/kie/guvnor/editors/drltext/BackendPlaceHolder.java
+++
@@ -1,8 +0,0 @@
-package org.kie.guvnor.editors.drltext;
-
-/**
- * We will eventually need a backend for validation services.
- */
-public class BackendPlaceHolder {
-
-}
--- guvnor-ng/guvnor-editors/guvnor-enum-editor/guvnor-enum-editor-backend/target/classes/org/kie/guvnor/editors/enums/BackendPlaceHolder.java
+++
@@ -1,8 +0,0 @@
-package org.kie.guvnor.editors.enums;
-
-/**
- * We will eventually need a backend for validation services.
- */
-public class BackendPlaceHolder {
-
-}
