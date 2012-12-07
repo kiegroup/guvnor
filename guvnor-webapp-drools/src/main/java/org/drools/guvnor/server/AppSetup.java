e7a6e58a62abf6ad2a115527a8c95a1ba4fb717b
Michael Anstis <michael.anstis@gmail.com>
2012-12-07 10:51:28 +0000

    Fix runtime issues

--- guvnor-webapp-drools/src/main/java/org/drools/guvnor/server/AppSetup.java
+++ guvnor-webapp-drools/src/main/java/org/drools/guvnor/server/AppSetup.java
@@ -8,46 +8,56 @@ import javax.enterprise.inject.Produces;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
+import org.kie.commons.io.IOService;
+import org.kie.commons.io.impl.IOServiceDotFileImpl;
 import org.kie.commons.java.nio.file.FileSystemAlreadyExistsException;
-import org.kie.commons.java.nio.file.FileSystems;
 import org.uberfire.backend.vfs.ActiveFileSystems;
 import org.uberfire.backend.vfs.Path;
-import org.uberfire.backend.vfs.PathFactory;
 import org.uberfire.backend.vfs.impl.ActiveFileSystemsImpl;
 import org.uberfire.backend.vfs.impl.FileSystemImpl;
 
 import static java.util.Arrays.*;
+import static org.kie.commons.io.FileSystemType.Bootstrap.*;
+import static org.uberfire.backend.vfs.PathFactory.*;
 
 @Singleton
 public class AppSetup {
 
-    private ActiveFileSystems fileSystems = new ActiveFileSystemsImpl();
+    private final IOService ioService = new IOServiceDotFileImpl();
+    private final ActiveFileSystems activeFileSystems = new ActiveFileSystemsImpl();
 
     @PostConstruct
     public void onStartup() {
         final String gitURL = "https://github.com/guvnorngtestuser1/guvnorng-playground.git";
         final String userName = "guvnorngtestuser1";
         final String password = "test1234";
-        final URI fsURI = URI.create("git://uf-playground");
+        final URI fsURI = URI.create( "git://uf-playground" );
 
-        final Map<String, Object> env = new HashMap<String, Object>();
-        env.put("username", userName);
-        env.put("password", password);
-        env.put("origin", gitURL);
+        final Map<String, Object> env = new HashMap<String, Object>() {{
+            put( "username", userName );
+            put( "password", password );
+            put( "origin", gitURL );
+        }};
 
         try {
-            FileSystems.newFileSystem(fsURI, env);
-        } catch (FileSystemAlreadyExistsException ex) {
+            ioService.newFileSystem( fsURI, env, BOOTSTRAP_INSTANCE );
+        } catch ( FileSystemAlreadyExistsException ex ) {
         }
 
-        final Path root = PathFactory.newPath( "uf-playground", "default://uf-playground" );
-
-        fileSystems.addBootstrapFileSystem(new FileSystemImpl(asList(root)));
+        final Path root = newPath( "uf-playground", "default://uf-playground" );
+        activeFileSystems.addBootstrapFileSystem( new FileSystemImpl( asList( root ) ) );
     }
 
-    @Produces @Named("fs")
+    @Produces
+    @Named("ioStrategy")
+    public IOService ioService() {
+        return ioService;
+    }
+
+    @Produces
+    @Named("fs")
     public ActiveFileSystems fileSystems() {
-        return fileSystems;
+        return activeFileSystems;
     }
 
 }
