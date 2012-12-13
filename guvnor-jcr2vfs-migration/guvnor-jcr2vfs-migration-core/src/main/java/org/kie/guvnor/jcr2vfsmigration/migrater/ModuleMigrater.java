c0aa31ea5dbd104f21df08a2bb348a5d7308403f
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2012-12-13 18:08:32 +0100

    add jcrRepositoryModuleService

--- guvnor-jcr2vfs-migration/guvnor-jcr2vfs-migration-core/src/main/java/org/kie/guvnor/jcr2vfsmigration/migrater/ModuleMigrater.java
+++ guvnor-jcr2vfs-migration/guvnor-jcr2vfs-migration-core/src/main/java/org/kie/guvnor/jcr2vfsmigration/migrater/ModuleMigrater.java
@@ -1,7 +1,9 @@
 package org.kie.guvnor.jcr2vfsmigration.migrater;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
+import org.drools.guvnor.server.RepositoryModuleService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,8 +12,8 @@ public class ModuleMigrater {
 
     protected static final Logger logger = LoggerFactory.getLogger(ModuleMigrater.class);
 
-//    @Inject
-//    protected RepositoryModuleService jcrRepositoryModuleService;
+    @Inject
+    protected RepositoryModuleService jcrRepositoryModuleService;
 
     public void migrateAll() {
         logger.info("  Module migration started");
