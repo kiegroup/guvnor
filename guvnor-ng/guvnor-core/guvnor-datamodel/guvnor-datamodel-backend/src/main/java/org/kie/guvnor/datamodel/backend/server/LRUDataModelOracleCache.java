5a96c5d7847a1e8d926977ef34212c31eb4a0caf
Michael Anstis <michael.anstis@gmail.com>
2013-01-02 20:12:33 +0000

    Implement simple LRU cache for DataModelOracles

--- guvnor-ng/guvnor-core/guvnor-datamodel/guvnor-datamodel-backend/src/main/java/org/kie/guvnor/datamodel/backend/server/LRUDataModelOracleCache.java
+++ guvnor-ng/guvnor-core/guvnor-datamodel/guvnor-datamodel-backend/src/main/java/org/kie/guvnor/datamodel/backend/server/LRUDataModelOracleCache.java
@@ -1,6 +1,7 @@
 package org.kie.guvnor.datamodel.backend.server;
 
-import java.util.HashMap;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
@@ -11,23 +12,36 @@ import org.kie.guvnor.datamodel.oracle.DataModelOracle;
 import org.uberfire.backend.vfs.Path;
 
 /**
- * A simple cache (no expiry) of DataModelOracles
+ * A simple LRU cache of DataModelOracles
  */
 @ApplicationScoped
-public class DefaultDataModelCache implements DataModelOracleCache {
+public class LRUDataModelOracleCache implements DataModelOracleCache {
 
-    private Map<Path, DataModelOracle> oracleCache = new HashMap<Path, DataModelOracle>();
+    private static final int MAX_ENTRIES = 20;
+
+    private Map<Path, DataModelOracle> oracleCache;
+
+    public LRUDataModelOracleCache() {
+        oracleCache = new LinkedHashMap<Path, DataModelOracle>( MAX_ENTRIES + 1,
+                                                                0.75f,
+                                                                true ) {
+            public boolean removeEldestEntry( Map.Entry eldest ) {
+                return size() > MAX_ENTRIES;
+            }
+        };
+        oracleCache = (Map) Collections.synchronizedMap( oracleCache );
+    }
 
     @Override
-    public synchronized DataModelOracle getDataModelOracle( final Path project ) {
+    public DataModelOracle getDataModelOracle( final Path project ) {
         PortablePreconditions.checkNotNull( "project",
                                             project );
         return oracleCache.get( project );
     }
 
     @Override
-    public synchronized void setDataModelOracle( final Path project,
-                                                 final DataModelOracle oracle ) {
+    public void setDataModelOracle( final Path project,
+                                    final DataModelOracle oracle ) {
         PortablePreconditions.checkNotNull( "project",
                                             project );
         PortablePreconditions.checkNotNull( "oracle",
@@ -37,12 +51,12 @@ public class DefaultDataModelCache implements DataModelOracleCache {
     }
 
     @Override
-    public synchronized void invalidateCache() {
+    public void invalidateCache() {
         this.oracleCache.clear();
     }
 
     @Override
-    public synchronized void invalidateCache( final Path project ) {
+    public void invalidateCache( final Path project ) {
         PortablePreconditions.checkNotNull( "project",
                                             project );
         this.oracleCache.remove( project );
