ed83221a40c213aa49ebc37623c8e9625cf44bb6
Toni Rikkola <toni@rikkola.net>
2013-01-17 13:20:27 +0200

    source service for pom.xml

---
+++ guvnor-ng/guvnor-commons/guvnor-commons-builder/src/main/java/org/kie/guvnor/builder/PomSourceService.java
@@ -0,0 +1,44 @@
+package org.kie.guvnor.builder;
+
+import org.kie.commons.io.IOService;
+import org.kie.commons.java.nio.file.Path;
+import org.kie.guvnor.commons.service.source.BaseSourceService;
+import org.kie.guvnor.commons.service.source.SourceContext;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+
+/**
+ * Source provider for KModule.xml
+ */
+public class PomSourceService
+        extends BaseSourceService {
+
+    private static final String PATTERN = "pom.xml";
+
+    @Inject
+    @Named("ioStrategy")
+    private IOService ioService;
+
+    protected PomSourceService() {
+        super("/");
+    }
+
+    @Override
+    public SourceContext getSource(final Path path) {
+        final String kmodule = ioService.readAllString(path);
+        final ByteArrayInputStream is = new ByteArrayInputStream(kmodule.getBytes());
+        final BufferedInputStream bis = new BufferedInputStream(is);
+        final SourceContext context = new SourceContext(bis,
+                stripProjectPrefix(path));
+        return context;
+    }
+
+    @Override
+    public String getPattern() {
+        return PATTERN;
+    }
+
+}
