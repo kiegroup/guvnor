fe3b7c803aded675fc9b7d90d35af75113f35aba
Michael Anstis <michael.anstis@gmail.com>
2013-02-28 16:22:33 +0000

    Move FactModel<->DRL conversion to separate class

--- guvnor-ng/guvnor-editors/guvnor-factmodel-editor/guvnor-factmodel-editor-backend/src/main/java/org/kie/guvnor/factmodel/backend/server/FactModelServiceImpl.java
+++ guvnor-ng/guvnor-editors/guvnor-factmodel-editor/guvnor-factmodel-editor-backend/src/main/java/org/kie/guvnor/factmodel/backend/server/FactModelServiceImpl.java
@@ -16,23 +16,13 @@
 
 package org.kie.guvnor.factmodel.backend.server;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.drools.compiler.DrlParser;
-import org.drools.compiler.DroolsParserException;
-import org.drools.guvnor.models.commons.backend.imports.ImportsParser;
-import org.drools.guvnor.models.commons.shared.imports.Imports;
-import org.drools.lang.descr.AnnotationDescr;
-import org.drools.lang.descr.PackageDescr;
-import org.drools.lang.descr.TypeDeclarationDescr;
-import org.drools.lang.descr.TypeFieldDescr;
 import org.jboss.errai.bus.server.annotations.Service;
 import org.kie.commons.io.IOService;
 import org.kie.commons.java.nio.base.options.CommentedOption;
@@ -42,11 +32,10 @@ import org.kie.guvnor.commons.service.verification.model.AnalysisReport;
 import org.kie.guvnor.datamodel.events.InvalidateDMOProjectCacheEvent;
 import org.kie.guvnor.datamodel.oracle.DataModelOracle;
 import org.kie.guvnor.datamodel.service.DataModelService;
-import org.kie.guvnor.factmodel.model.AnnotationMetaModel;
+import org.kie.guvnor.factmodel.backend.server.util.FactModelPersistence;
 import org.kie.guvnor.factmodel.model.FactMetaModel;
 import org.kie.guvnor.factmodel.model.FactModelContent;
 import org.kie.guvnor.factmodel.model.FactModels;
-import org.kie.guvnor.factmodel.model.FieldMetaModel;
 import org.kie.guvnor.factmodel.service.FactModelService;
 import org.kie.guvnor.services.file.CopyService;
 import org.kie.guvnor.services.file.DeleteService;
@@ -118,7 +107,7 @@ public class FactModelServiceImpl implements FactModelService {
                                             false );
 
         ioService.write( paths.convert( newPath ),
-                         marshal( content ),
+                         FactModelPersistence.marshal( content ),
                          makeCommentedOption( comment ) );
 
         //Signal creation to interested parties
@@ -134,7 +123,7 @@ public class FactModelServiceImpl implements FactModelService {
         //Signal opening to interested parties
         resourceOpenedEvent.fire( new ResourceOpenedEvent( path ) );
 
-        return unmarshal( content );
+        return FactModelPersistence.unmarshal( content );
     }
 
     @Override
@@ -160,7 +149,7 @@ public class FactModelServiceImpl implements FactModelService {
                                             false );
 
         ioService.write( paths.convert( newPath ),
-                         marshal( content ),
+                         FactModelPersistence.marshal( content ),
                          makeCommentedOption( comment ) );
 
         //Invalidate Project-level DMO cache as Model has changed.
@@ -178,7 +167,7 @@ public class FactModelServiceImpl implements FactModelService {
                       final Metadata metadata,
                       final String comment ) {
         ioService.write( paths.convert( resource ),
-                         marshal( content ),
+                         FactModelPersistence.marshal( content ),
                          metadataService.setUpAttributes( resource,
                                                           metadata ),
                          makeCommentedOption( comment ) );
@@ -220,7 +209,8 @@ public class FactModelServiceImpl implements FactModelService {
     @Override
     public String toSource( final Path path,
                             final FactModels model ) {
-        return sourceServices.getServiceFor( paths.convert( path ) ).getSource( paths.convert( path ), marshal( model ) );
+        return sourceServices.getServiceFor( paths.convert( path ) ).getSource( paths.convert( path ),
+                                                                                FactModelPersistence.marshal( model ) );
     }
 
     @Override
@@ -251,127 +241,6 @@ public class FactModelServiceImpl implements FactModelService {
                                                         commitMessage,
                                                         when );
         return co;
-    }
-
-    private String marshal( final FactModels content ) {
-        final StringBuilder sb = new StringBuilder();
-        sb.append( content.getImports().toString() );
-        sb.append( "\n" );
-        for ( final FactMetaModel factMetaModel : content.getModels() ) {
-            sb.append( toDRL( factMetaModel ) ).append( "\n\n" );
-        }
-        return sb.toString().trim();
-    }
-
-    private FactModels unmarshal( final String content ) {
-        try {
-            final List<FactMetaModel> models = toModel( content );
-            final FactModels factModels = new FactModels();
-            factModels.getModels().addAll( models );
-
-            //De-serialize imports
-            final Imports imports = ImportsParser.parseImports( content );
-            factModels.setImports( imports );
-            return factModels;
-
-        } catch ( final DroolsParserException e ) {
-            throw new RuntimeException( e );
-        }
-        //TODO {porcelli} needs define error handling strategy
-//            log.error( "Unable to parse the DRL for the model - falling back to text (" + e.getMessage() + ")" );
-//            RuleContentText text = new RuleContentText();
-//            text.content = item.getContent();
-//            asset.setContent( text );
-    }
-
-    private List<FactMetaModel> toModel( String drl )
-            throws DroolsParserException {
-        if ( drl != null && ( drl.startsWith( "#advanced" ) || drl.startsWith( "//advanced" ) ) ) {
-            throw new DroolsParserException( "Using advanced editor" );
-        }
-        final DrlParser parser = new DrlParser();
-        final PackageDescr pkg = parser.parse( drl );
-        if ( parser.hasErrors() ) {
-            throw new DroolsParserException( "The model drl " + drl + " is not valid" );
-        }
-
-        if ( pkg == null ) {
-            return emptyList();
-        }
-        final List<TypeDeclarationDescr> types = pkg.getTypeDeclarations();
-        final List<FactMetaModel> list = new ArrayList<FactMetaModel>( types.size() );
-        for ( final TypeDeclarationDescr td : types ) {
-            final FactMetaModel mm = new FactMetaModel();
-            mm.setName( td.getTypeName() );
-            mm.setSuperType( td.getSuperTypeName() );
-
-            final Map<String, TypeFieldDescr> fields = td.getFields();
-            for ( Map.Entry<String, TypeFieldDescr> en : fields.entrySet() ) {
-                final String fieldName = en.getKey();
-                final TypeFieldDescr descr = en.getValue();
-                final FieldMetaModel fm = new FieldMetaModel( fieldName,
-                                                              descr.getPattern().getObjectType() );
-
-                mm.getFields().add( fm );
-            }
-
-            final Map<String, AnnotationDescr> annotations = td.getAnnotations();
-            for ( final Map.Entry<String, AnnotationDescr> en : annotations.entrySet() ) {
-                final String annotationName = en.getKey();
-                final AnnotationDescr descr = en.getValue();
-                final Map<String, String> values = descr.getValues();
-                final AnnotationMetaModel am = new AnnotationMetaModel( annotationName,
-                                                                        values );
-
-                mm.getAnnotations().add( am );
-            }
-
-            list.add( mm );
-        }
-
-        return list;
-    }
-
-    private String toDRL( FactMetaModel mm ) {
-        final StringBuilder sb = new StringBuilder();
-        sb.append( "declare " ).append( mm.getName() );
-        if ( mm.hasSuperType() ) {
-            sb.append( " extends " );
-            sb.append( mm.getSuperType() );
-        }
-        for ( int i = 0; i < mm.getAnnotations().size(); i++ ) {
-            AnnotationMetaModel a = mm.getAnnotations().get( i );
-            sb.append( "\n\t" );
-            sb.append( buildAnnotationDRL( a ) );
-        }
-        for ( int i = 0; i < mm.getFields().size(); i++ ) {
-            FieldMetaModel f = mm.getFields().get( i );
-            sb.append( "\n\t" );
-            sb.append( f.name ).append( ": " ).append( f.type );
-        }
-        sb.append( "\nend" );
-        return sb.toString();
-    }
-
-    private StringBuilder buildAnnotationDRL( AnnotationMetaModel a ) {
-        final StringBuilder sb = new StringBuilder();
-        sb.append( "@" );
-        sb.append( a.name );
-        sb.append( "(" );
-        for ( final Map.Entry<String, String> e : a.getValues().entrySet() ) {
-            if ( e.getKey() != null && e.getKey().length() > 0 ) {
-                sb.append( e.getKey() );
-                sb.append( " = " );
-            }
-            if ( e.getValue() != null && e.getValue().length() > 0 ) {
-                sb.append( e.getValue() );
-            }
-            sb.append( ", " );
-        }
-        sb.delete( sb.length() - 2,
-                   sb.length() );
-        sb.append( ")" );
-        return sb;
     }
 
 }
