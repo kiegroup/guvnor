96bf91cdc86aadff887dd3bc91092975a3761374
Tihomir Surdilovic <tsurdilo@redhat.com>
2013-03-05 10:08:46 -0500

    BZ-917003 - The jbpm.xml file in designer.war doesn't handle passwords with special characters that can be part of URLs for the REST call

--- guvnor-webapp/src/main/java/org/drools/guvnor/server/files/OryxEditorServlet.java
+++ guvnor-webapp/src/main/java/org/drools/guvnor/server/files/OryxEditorServlet.java
@@ -5,21 +5,18 @@ import org.drools.guvnor.client.rpc.RuleFlowContentModel;
 import org.drools.guvnor.server.RepositoryServiceServlet;
 import org.drools.guvnor.server.util.LoggingHelper;
 import org.jboss.seam.contexts.Contexts;
-import org.jboss.seam.security.Identity;
 
-import javax.security.auth.login.LoginException;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class OryxEditorServlet extends HttpServlet {
+public class OryxEditorServlet extends RepositoryServlet {
 
     private static final LoggingHelper log = LoggingHelper.getLogger(OryxEditorServlet.class);
 
-    public void service(HttpServletRequest request,
-                        HttpServletResponse response)
+    public void service(final HttpServletRequest request,
+                        final HttpServletResponse response)
             throws ServletException,
             IOException {
         log.debug("Incoming request from Oryx Designer:" + request.getRequestURL());
@@ -28,59 +25,38 @@ public class OryxEditorServlet extends HttpServlet {
             throw new ServletException("No application context active.");
         }
 
-        //action never used. Why? - JT
-        String action = request.getParameter("action");
-        String uuid = request.getParameter("uuid");
-        String usr = request.getParameter("usr");
-        String pwd = request.getParameter("pwd");
+        doAuthorizedAction(request,
+                response,
+                new Command() {
+                    public void execute() throws Exception {
+                        String uuid = request.getParameter("uuid");
+                        try {
+                            RuleAsset asset = RepositoryServiceServlet.getAssetService().loadRuleAsset(uuid);
+                            if (asset.getContent() != null) {
+                                response.setContentType("application/xml");
+                                response.setCharacterEncoding("UTF-8");
+                                String content = asset.getContent().toString();
+                                if (asset.getContent() instanceof RuleFlowContentModel) {
+                                    content = ((RuleFlowContentModel) asset.getContent()).getXml();
+                                }
 
-        if (uuid == null) {
-            throw new ServletException(new IllegalArgumentException("Parameter uuid not specified."));
-        }
-        //action never used. Why? - JT
-        /*if (action == null) {
-            action = "json";
-        } */
+                                if (content != null) {
+                                    response.getOutputStream().write(content.getBytes("UTF-8"));
+                                    response.getOutputStream().close();
+                                } else {
+                                    setDefaultResponse(response);
+                                }
 
-        // log in
-        Identity ids = Identity.instance();
-        ids.getCredentials().setUsername(usr);
-        ids.getCredentials().setPassword(pwd);
-
-        try {
-            ids.authenticate();
-        } catch (LoginException e) {
-            throw new ServletException(new IllegalArgumentException("Unable to authenticate user."));
-        }
-
-        log.debug("Successful login");
-
-        try {
-            RuleAsset asset = RepositoryServiceServlet.getAssetService().loadRuleAsset(uuid);
-            if (asset.getContent() != null) {
-                response.setContentType("application/xml");
-                response.setCharacterEncoding("UTF-8");
-                String content = asset.getContent().toString();
-                if (asset.getContent() instanceof RuleFlowContentModel) {
-                    content = ((RuleFlowContentModel) asset.getContent()).getXml();
-                }
-
-                if (content != null) {
-                    response.getOutputStream().write(content.getBytes("UTF-8"));
-                    response.getOutputStream().close();
-                } else {
-                    setDefaultResponse(response);
-                }
-
-            } else {
-                setDefaultResponse(response);
-            }
-        } catch (Throwable t) {
-            log.error(t.getMessage(),
-                    t);
-            setDefaultResponse(response);
-        }
-
+                            } else {
+                                setDefaultResponse(response);
+                            }
+                        } catch (Throwable t) {
+                            log.error(t.getMessage(),
+                                    t);
+                            setDefaultResponse(response);
+                        }
+                    }
+                });
     }
 
     private void setDefaultResponse(HttpServletResponse response) throws ServletException {
