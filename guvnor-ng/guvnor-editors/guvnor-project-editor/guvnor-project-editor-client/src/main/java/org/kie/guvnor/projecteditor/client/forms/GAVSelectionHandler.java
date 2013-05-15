8f9c94df6f9b4d2649ad9ea7e692affc21299928
Toni Rikkola <toni@rikkola.net>
2013-05-15 13:24:01 +0300

    moving project screen and project imports editor

--- guvnor-ng/guvnor-editors/guvnor-project-editor/guvnor-project-editor-client/src/main/java/org/kie/guvnor/projecteditor/client/forms/GAVSelectionHandler.java
+++
@@ -1,9 +0,0 @@
-package org.kie.guvnor.projecteditor.client.forms;
-
-import org.kie.guvnor.project.model.GAV;
-
-public interface GAVSelectionHandler {
-
-    void onSelection(GAV gav);
-
-}
--- guvnor-ng/guvnor-editors/guvnor-project-editor/guvnor-project-editor-client/src/main/java/org/kie/guvnor/projecteditor/client/wizard/NewProjectWizardContext.java
+++
@@ -1,7 +0,0 @@
-package org.kie.guvnor.projecteditor.client.wizard;
-
-import org.uberfire.client.wizards.WizardContext;
-
-public class NewProjectWizardContext
-        implements WizardContext {
-}
