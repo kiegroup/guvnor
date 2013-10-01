8d4d334fca65fb67c5b592cdcd73718a4ae6c5c7
Michael Anstis <michael.anstis@gmail.com>
2013-10-01 21:18:52 +0100

    BZ1014071 - DataModelOracle: Performance issues when model is large

--- guvnor-project/guvnor-project-backend/src/test/java/org/guvnor/common/services/project/backend/server/ProjectConfigurationContentHandlerTest.java
+++ guvnor-project/guvnor-project-backend/src/test/java/org/guvnor/common/services/project/backend/server/ProjectConfigurationContentHandlerTest.java
@@ -1,6 +1,6 @@
 package org.guvnor.common.services.project.backend.server;
 
-import org.drools.workbench.models.commons.shared.imports.Import;
+import org.drools.workbench.models.datamodel.imports.Import;
 import org.guvnor.common.services.project.model.ProjectImports;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,7 +8,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ProjectConfigurationContentHandlerTest {
-
 
     private ProjectConfigurationContentHandler handler;
 
@@ -19,25 +18,25 @@ public class ProjectConfigurationContentHandlerTest {
 
     @Test
     public void testVersion() throws Exception {
-        assertTrue(handler.toString(new ProjectImports()).contains("<version>1.0</version>"));
+        assertTrue( handler.toString( new ProjectImports() ).contains( "<version>1.0</version>" ) );
     }
 
     @Test
     public void testEmptyImports() throws Exception {
-        String xml = handler.toString(new ProjectImports());
-        assertTrue(xml.contains("<imports>"));
-        assertTrue(xml.contains("</imports>"));
+        String xml = handler.toString( new ProjectImports() );
+        assertTrue( xml.contains( "<imports>" ) );
+        assertTrue( xml.contains( "</imports>" ) );
     }
 
     @Test
     public void testImports() throws Exception {
         ProjectImports configuration = new ProjectImports();
-        configuration.getImports().addImport(new Import("java.util.List"));
-        configuration.getImports().addImport(new Import("org.test.Object"));
-        String xml = handler.toString(configuration);
-        assertTrue(xml.contains("<import>"));
-        assertTrue(xml.contains("</import>"));
-        assertTrue(xml.contains("<type>java.util.List</type>"));
-        assertTrue(xml.contains("<type>org.test.Object</type>"));
+        configuration.getImports().addImport( new Import( "java.util.List" ) );
+        configuration.getImports().addImport( new Import( "org.test.Object" ) );
+        String xml = handler.toString( configuration );
+        assertTrue( xml.contains( "<import>" ) );
+        assertTrue( xml.contains( "</import>" ) );
+        assertTrue( xml.contains( "<type>java.util.List</type>" ) );
+        assertTrue( xml.contains( "<type>org.test.Object</type>" ) );
     }
 }
