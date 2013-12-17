fbc80bc44c6bb4f710d2548395ad3f3f49837226
Toni Rikkola <toni@rikkola.net>
2013-12-17 14:03:16 +0200

    BZ-1039916 - Editor for project.imports file incomplete, Restore button saves

--- guvnor-services-api/src/main/java/org/guvnor/common/services/shared/metadata/CategoriesService.java
+++ guvnor-services-api/src/main/java/org/guvnor/common/services/shared/metadata/CategoriesService.java
@@ -1,17 +1,18 @@
 package org.guvnor.common.services.shared.metadata;
 
+import org.guvnor.common.services.shared.file.SupportsCreate;
+import org.guvnor.common.services.shared.file.SupportsRead;
+import org.guvnor.common.services.shared.file.SupportsUpdate;
 import org.guvnor.common.services.shared.metadata.model.Categories;
 import org.guvnor.common.services.shared.metadata.model.CategoriesModelContent;
 import org.jboss.errai.bus.server.annotations.Remote;
 import org.uberfire.backend.vfs.Path;
 
 @Remote
-public interface CategoriesService {
-
-    void save( final Path path,
-               final Categories content );
-
-    Categories getContent( final Path path );
+public interface CategoriesService
+    extends
+        SupportsRead<Categories>,
+        SupportsUpdate<Categories> {
 
     Categories getCategoriesFromResource( final Path resource );
 
--- guvnor-services-backend/src/main/java/org/guvnor/common/services/backend/metadata/CategoryServiceImpl.java
+++ guvnor-services-backend/src/main/java/org/guvnor/common/services/backend/metadata/CategoryServiceImpl.java
@@ -1,5 +1,6 @@
 package org.guvnor.common.services.backend.metadata;
 
+import java.util.Date;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -7,16 +8,22 @@ import javax.inject.Named;
 import com.thoughtworks.xstream.XStream;
 import org.guvnor.common.services.backend.exceptions.ExceptionUtilities;
 import org.guvnor.common.services.shared.metadata.CategoriesService;
+import org.guvnor.common.services.shared.metadata.MetadataService;
 import org.guvnor.common.services.shared.metadata.model.Categories;
 import org.guvnor.common.services.shared.metadata.model.CategoriesModelContent;
+import org.guvnor.common.services.shared.metadata.model.Metadata;
 import org.jboss.errai.bus.server.annotations.Service;
 import org.uberfire.backend.server.util.Paths;
 import org.uberfire.backend.vfs.Path;
 import org.uberfire.io.IOService;
+import org.uberfire.java.nio.base.options.CommentedOption;
+import org.uberfire.rpc.SessionInfo;
+import org.uberfire.security.Identity;
 
 @Service
 @ApplicationScoped
-public class CategoryServiceImpl implements CategoriesService {
+public class CategoryServiceImpl
+        implements CategoriesService {
 
     private final XStream xt = new XStream();
 
@@ -24,69 +31,101 @@ public class CategoryServiceImpl implements CategoriesService {
     @Named("ioStrategy")
     private IOService ioService;
 
+    @Inject
+    private MetadataService metadataService;
+
+    @Inject
+    private Identity identity;
+
+    @Inject
+    private SessionInfo sessionInfo;
+
     @Override
-    public void save( final Path path,
-                      final Categories content ) {
-        saveWithServerSidePath( Paths.convert( path ),
-                                content );
+    public Path save(Path path, Categories content, Metadata metadata, String comment) {
+        return Paths.convert(saveWithServerSidePath(Paths.convert(path),
+                content, metadata, comment));
     }
 
-    private void saveWithServerSidePath( org.uberfire.java.nio.file.Path path,
-                                         Categories content ) {
-        try {
-            ioService.write( path,
-                             xt.toXML( content ) );
+    private org.uberfire.java.nio.file.Path saveWithServerSidePath(org.uberfire.java.nio.file.Path path,
+            Categories content) {
+        return saveWithServerSidePath(path, content, null, "");
+    }
 
-        } catch ( Exception e ) {
-            throw ExceptionUtilities.handleException( e );
+    private org.uberfire.java.nio.file.Path saveWithServerSidePath(
+            org.uberfire.java.nio.file.Path path,
+            Categories content,
+            Metadata metadata,
+            String comment) {
+        try {
+
+            ioService.write(path,
+                    xt.toXML(content),
+                    metadataService.setUpAttributes(Paths.convert(path),
+                            metadata),
+                    makeCommentedOption(comment));
+
+            return path;
+        } catch (Exception e) {
+            throw ExceptionUtilities.handleException(e);
         }
     }
 
+    private CommentedOption makeCommentedOption( final String commitMessage ) {
+        final String name = identity.getName();
+        final Date when = new Date();
+        return new CommentedOption( sessionInfo.getId(),
+                name,
+                null,
+                commitMessage,
+                when );
+    }
+
     @Override
-    public Categories getContent( final Path path ) {
+    public Categories load(Path path) {
         try {
-            final String content = ioService.readAllString( Paths.convert( path ) );
+            final String content = ioService.readAllString(Paths.convert(path));
             final Categories categories;
 
-            if ( content.trim().equals( "" ) ) {
+            if (content.trim().equals("")) {
                 categories = new Categories();
             } else {
-                categories = (Categories) xt.fromXML( content );
+                categories = (Categories) xt.fromXML(content);
             }
 
             return categories;
 
-        } catch ( Exception e ) {
-            throw ExceptionUtilities.handleException( e );
+        } catch (Exception e) {
+            throw ExceptionUtilities.handleException(e);
         }
     }
 
     @Override
-    public CategoriesModelContent getContentByRoot( Path pathToRoot ) {
+    public CategoriesModelContent getContentByRoot(Path pathToRoot) {
         CategoriesModelContent categoriesModelContent = new CategoriesModelContent();
 
-        org.uberfire.java.nio.file.Path path = Paths.convert( pathToRoot ).resolve( "categories.xml" );
-        if ( !ioService.exists( path ) ) {
-            saveWithServerSidePath( path, new Categories() );
+        org.uberfire.java.nio.file.Path path = Paths.convert(pathToRoot).resolve("categories.xml");
+        if (!ioService.exists(path)) {
+            saveWithServerSidePath(path, new Categories());
         }
 
-        Path categoriesPath = Paths.convert( path );
+        Path categoriesPath = Paths.convert(path);
 
-        categoriesModelContent.setPath( categoriesPath );
-        categoriesModelContent.setCategories( getContent( categoriesPath ) );
+        categoriesModelContent.setPath(categoriesPath);
+        categoriesModelContent.setCategories(load(categoriesPath));
 
         return categoriesModelContent;
     }
 
     @Override
-    public Categories getCategoriesFromResource( final Path resource ) {
+    public Categories getCategoriesFromResource(final Path resource) {
         try {
-            final org.uberfire.java.nio.file.Path categoriesPath = Paths.convert( resource ).getRoot().resolve( "categories.xml" );
+            final org.uberfire.java.nio.file.Path categoriesPath = Paths.convert(resource).getRoot().resolve("categories.xml");
 
-            return getContent( Paths.convert( categoriesPath ) );
+            return load(Paths.convert(categoriesPath));
 
-        } catch ( Exception e ) {
-            throw ExceptionUtilities.handleException( e );
+        } catch (Exception e) {
+            throw ExceptionUtilities.handleException(e);
         }
     }
+
 }
