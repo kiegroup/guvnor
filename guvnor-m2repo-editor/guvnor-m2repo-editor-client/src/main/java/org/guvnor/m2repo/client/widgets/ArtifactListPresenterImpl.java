edcc5d3a961d0a4180fbff599a7184c8eccdb642
Michael Anstis <michael.anstis@gmail.com>
2014-12-03 13:21:11 +0000

    BZ1091983: Paging in Artifact repository does not work
    (cherry picked from commit 858684c7f47995eb8ea00ac6961abe04104ff66e)

--- guvnor-m2repo-editor/guvnor-m2repo-editor-client/src/main/java/org/guvnor/m2repo/client/widgets/ArtifactListPresenterImpl.java
+++ guvnor-m2repo-editor/guvnor-m2repo-editor-client/src/main/java/org/guvnor/m2repo/client/widgets/ArtifactListPresenterImpl.java
@@ -27,6 +27,7 @@ import org.guvnor.m2repo.model.JarListPageRow;
 import org.guvnor.m2repo.service.M2RepoService;
 import org.jboss.errai.common.client.api.Caller;
 import org.jboss.errai.common.client.api.RemoteCallback;
+import org.uberfire.commons.validation.PortablePreconditions;
 import org.uberfire.paging.PageRequest;
 import org.uberfire.paging.PageResponse;
 import org.uberfire.workbench.events.NotificationEvent;
@@ -43,7 +44,7 @@ public class ArtifactListPresenterImpl
     @Inject
     private Event<NotificationEvent> notification;
 
-    private AsyncDataProvider<JarListPageRow> dataProvider;
+    private RefreshableAsyncDataProvider dataProvider;
 
     @PostConstruct
     public void init() {
@@ -52,27 +53,22 @@ public class ArtifactListPresenterImpl
 
     @Override
     public void refresh() {
-        dataProvider = new AsyncDataProvider<JarListPageRow>() {
-            protected void onRangeChanged( HasData<JarListPageRow> display ) {
-                final Range range = display.getVisibleRange();
-                PageRequest request = new PageRequest( range.getStart(),
-                                                       range.getLength() );
-
-                m2RepoService.call( new RemoteCallback<PageResponse<JarListPageRow>>() {
-                    @Override
-                    public void callback( final PageResponse<JarListPageRow> response ) {
-                        updateRowCount( response.getTotalRowSize(),
-                                        response.isTotalRowSizeExact() );
-                        updateRowData( response.getStartRowIndex(),
-                                       response.getPageRowList() );
-                        notification.fire( new NotificationEvent( M2RepoEditorConstants.INSTANCE.RefreshedSuccessfully() ) );
-                    }
-                } ).listJars( request, view.getCurrentFilter() );
+        // We couldn't attach the Display to the DataProvider in the @PostConstruct method
+        // as this initializes the Display with data using AsyncDataProvider.onRangeChanged()
+        // at which time the View may not have been initialised fully.
+        if ( dataProvider == null ) {
+            dataProvider = new RefreshableAsyncDataProvider( view,
+                                                             m2RepoService );
+            dataProvider.addDataDisplay( view.getDisplay() );
+        } else {
+            for ( HasData<JarListPageRow> display : dataProvider.getDataDisplays() ) {
+                dataProvider.refresh( display );
             }
-        };
-        dataProvider.addDataDisplay( view.getDisplay() );
+            notification.fire( new NotificationEvent( M2RepoEditorConstants.INSTANCE.RefreshedSuccessfully() ) );
+        }
     }
 
+    @Override
     public void search( final String filter ) {
         view.setCurrentFilter( filter );
         refresh();
@@ -80,5 +76,44 @@ public class ArtifactListPresenterImpl
 
     public ArtifactListViewImpl getView() {
         return view;
+    }
+
+    /**
+     * Extension to AsyncDataProvider that supports refreshing the table
+     */
+    private static class RefreshableAsyncDataProvider extends AsyncDataProvider<JarListPageRow> {
+
+        private ArtifactListViewImpl view;
+        private Caller<M2RepoService> m2RepoService;
+
+        protected RefreshableAsyncDataProvider( final ArtifactListViewImpl view,
+                                                final Caller<M2RepoService> m2RepoService ) {
+            this.view = PortablePreconditions.checkNotNull( "view",
+                                                            view );
+            this.m2RepoService = PortablePreconditions.checkNotNull( "m2RepoService",
+                                                                     m2RepoService );
+        }
+
+        protected void refresh( final HasData<JarListPageRow> display ) {
+            onRangeChanged( display );
+        }
+
+        @Override
+        protected void onRangeChanged( HasData<JarListPageRow> display ) {
+            final Range range = display.getVisibleRange();
+            PageRequest request = new PageRequest( range.getStart(),
+                                                   range.getLength() );
+
+            m2RepoService.call( new RemoteCallback<PageResponse<JarListPageRow>>() {
+                @Override
+                public void callback( final PageResponse<JarListPageRow> response ) {
+                    updateRowCount( response.getTotalRowSize(),
+                                    response.isTotalRowSizeExact() );
+                    updateRowData( response.getStartRowIndex(),
+                                   response.getPageRowList() );
+                }
+            } ).listJars( request, view.getCurrentFilter() );
+        }
+
     }
 }
