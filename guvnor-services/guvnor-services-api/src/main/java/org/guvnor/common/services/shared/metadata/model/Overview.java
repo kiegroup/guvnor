52308e981f82c366467b0a0656aa5099ea353a3c
Toni Rikkola <toni@rikkola.net>
2015-02-03 16:11:25 +0200

    Bug 1179441 - Unsaved asset comments discarded without warning
    (cherry picked from commit 69ce6f6ad2d5638e9a24d2344c8c849e1d026f32)

--- guvnor-services/guvnor-services-api/src/main/java/org/guvnor/common/services/shared/metadata/model/Overview.java
+++ guvnor-services/guvnor-services-api/src/main/java/org/guvnor/common/services/shared/metadata/model/Overview.java
@@ -40,4 +40,33 @@ public class Overview {
         this.metadata = metadata;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Overview overview = (Overview) o;
+
+        if (metadata != null ? !metadata.equals(overview.metadata) : overview.metadata != null) {
+            return false;
+        }
+        if (projectName != null ? !projectName.equals(overview.projectName) : overview.projectName != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = metadata != null ? metadata.hashCode() : 0;
+        result = ~~result;
+        result = 31 * result + (projectName != null ? projectName.hashCode() : 0);
+        result = ~~result;
+        return result;
+    }
 }
