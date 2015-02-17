88a4111b8e5cbbba75026a23f90165d8029f7da1
Toni Rikkola <toni@rikkola.net>
2015-02-17 15:02:49 +0200

    Bug 1085393 - Test Case Runner should show the elapsed time like the 5 console

--- guvnor-services/guvnor-services-api/src/main/java/org/guvnor/common/services/shared/test/TestResultMessage.java
+++ guvnor-services/guvnor-services-api/src/main/java/org/guvnor/common/services/shared/test/TestResultMessage.java
@@ -3,24 +3,26 @@ package org.guvnor.common.services.shared.test;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.guvnor.common.services.shared.test.TestResultMessage;
 import org.jboss.errai.common.client.api.annotations.Portable;
 
 @Portable
 public class TestResultMessage {
 
     private String identifier;
-    private int runCount;
-    private List<Failure> failures;
+    private int    runCount;
+    private Long   runTime;
+    private List<Failure> failures = new ArrayList<Failure>();
 
     public TestResultMessage() {
     }
 
-    public TestResultMessage( String identifier,
-                              int runCount,
-                              List<Failure> failures) {
+    public TestResultMessage(String identifier,
+                             int runCount,
+                             long runTime,
+                             List<Failure> failures) {
         this.identifier = identifier;
         this.runCount = runCount;
+        this.runTime = runTime;
         this.failures = failures;
     }
 
@@ -36,28 +38,31 @@ public class TestResultMessage {
         return runCount;
     }
 
+    public long getRunTime() {
+        return runTime;
+    }
 
     public List<Failure> getFailures() {
         return failures;
     }
 
-    public List<String> getResultStrings() { 
+    public List<String> getResultStrings() {
         List<String> result = new ArrayList<String>(3 + (failures == null ? 0 : failures.size()));
-        result.add( "RunCount: " + this.runCount);
-        if( this.failures != null ) {
-            for ( Failure failure : this.failures ) { 
-                result.add( "Failure: " + failure.getMessage() );
+        result.add("RunCount: " + this.runCount);
+        if (this.failures != null) {
+            for (Failure failure : this.failures) {
+                result.add("Failure: " + failure.getMessage());
             }
         }
         return result;
     }
-    
+
     @Override
     public String toString() {
         return "TestResultMessage{" +
-                "identifier=" + identifier +
-                ", runCount=" + runCount +
-                ", failures=" + failures +
-                '}';
+               "identifier=" + identifier +
+               ", runCount=" + runCount +
+               ", failures=" + failures +
+               '}';
     }
 }
