e5704f8c98c0a7fff9eae62e58c8f14720998a92
pefernan <pere.fernandez@gmail.com>
2015-11-05 10:56:04 +0100

    BZ-1275620: AssetMgmt: Names of files to be promoted not visible

--- guvnor-asset-mgmt/guvnor-asset-mgmt-client/src/main/java/org/guvnor/asset/management/client/editors/forms/promote/SelectAssetsToPromoteViewImpl.java
+++ guvnor-asset-mgmt/guvnor-asset-mgmt-client/src/main/java/org/guvnor/asset/management/client/editors/forms/promote/SelectAssetsToPromoteViewImpl.java
@@ -69,10 +69,10 @@ public class SelectAssetsToPromoteViewImpl extends Composite implements SelectAs
     @UiField
     public CheckBox requiresReviewCheckBox;
 
-    @UiField(provided = true)
+    @UiField
     public ListBox filesInTheBranchList;
 
-    @UiField(provided = true)
+    @UiField
     public ListBox filesToPromoteList;
 
     @UiField
@@ -90,10 +90,6 @@ public class SelectAssetsToPromoteViewImpl extends Composite implements SelectAs
     private Map<String, String> commitsPerFile;
 
     public SelectAssetsToPromoteViewImpl() {
-
-        filesInTheBranchList = new ListBox( true );
-
-        filesToPromoteList = new ListBox( true );
 
         initWidget( uiBinder.createAndBindUi( this ) );
 
