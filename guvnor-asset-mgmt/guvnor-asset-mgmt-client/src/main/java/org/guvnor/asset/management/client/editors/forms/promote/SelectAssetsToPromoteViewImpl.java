81d526e5dacfeabf5cdb0b385de2a05c929f4ee1
pefernan <pere.fernandez@gmail.com>
2015-11-06 12:23:03 +0100

    BZ-1275621: AssetMgmt: Impossible to promote multiple selected files

--- guvnor-asset-mgmt/guvnor-asset-mgmt-client/src/main/java/org/guvnor/asset/management/client/editors/forms/promote/SelectAssetsToPromoteViewImpl.java
+++ guvnor-asset-mgmt/guvnor-asset-mgmt-client/src/main/java/org/guvnor/asset/management/client/editors/forms/promote/SelectAssetsToPromoteViewImpl.java
@@ -1,33 +1,21 @@
 /*
- * Copyright 2012 JBoss Inc
+ * Copyright 2015 JBoss Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+*/
 package org.guvnor.asset.management.client.editors.forms.promote;
-
-import java.util.HashMap;
-import java.util.Map;
-import javax.enterprise.context.Dependent;
-import javax.enterprise.event.Event;
-import javax.enterprise.event.Observes;
-import javax.inject.Inject;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.json.client.JSONArray;
-import com.google.gwt.json.client.JSONObject;
-import com.google.gwt.json.client.JSONParser;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -37,12 +25,8 @@ import org.gwtbootstrap3.client.ui.Button;
 import org.gwtbootstrap3.client.ui.CheckBox;
 import org.gwtbootstrap3.client.ui.ListBox;
 import org.gwtbootstrap3.client.ui.TextBox;
-import org.jboss.errai.security.shared.api.identity.User;
-import org.uberfire.client.mvp.PlaceManager;
-import org.uberfire.ext.widgets.common.client.forms.GetFormParamsEvent;
-import org.uberfire.ext.widgets.common.client.forms.RequestFormParamsEvent;
-import org.uberfire.ext.widgets.common.client.forms.SetFormParamsEvent;
-import org.uberfire.workbench.events.NotificationEvent;
+
+import javax.enterprise.context.Dependent;
 
 @Dependent
 public class SelectAssetsToPromoteViewImpl extends Composite implements SelectAssetsToPromotePresenter.SelectAssetsToPromoteView {
@@ -52,12 +36,7 @@ public class SelectAssetsToPromoteViewImpl extends Composite implements SelectAs
 
     }
 
-    private static Binder uiBinder = GWT.create( Binder.class );
-    @Inject
-    private User identity;
-
-    @Inject
-    private PlaceManager placeManager;
+    private static Binder uiBinder = GWT.create(Binder.class);
 
     private SelectAssetsToPromotePresenter presenter;
 
@@ -81,167 +60,90 @@ public class SelectAssetsToPromoteViewImpl extends Composite implements SelectAs
     @UiField
     public Button promoteSelectedFilesButton;
 
-    @Inject
-    private Event<NotificationEvent> notification;
-
-    @Inject
-    private Event<GetFormParamsEvent> getFormParamsEvent;
-
-    private Map<String, String> commitsPerFile;
-
     public SelectAssetsToPromoteViewImpl() {
 
         filesInTheBranchList = new ListBox();
         filesInTheBranchList.setMultipleSelect( true );
 
         filesToPromoteList = new ListBox();
-        filesToPromoteList.setMultipleSelect( true );
+        filesToPromoteList.setMultipleSelect(true);
 
-        initWidget( uiBinder.createAndBindUi( this ) );
-
-    }
-
-    public void getOutputMap( @Observes RequestFormParamsEvent event ) {
-
-        Map<String, Object> outputMap = new HashMap<String, Object>();
-
-        int filesToPromoteCount = getFilesToPromoteList().getItemCount();
-        String out_commits = "";
-        for ( int i = 0; i < filesToPromoteCount; i++ ) {
-            String commits = commitsPerFile.get( getFilesToPromoteList().getItemText( i ) );
-            if ( commits == null || commits.length() == 0 || out_commits.contains( commits ) ) {
-                continue;
-            }
-            if ( out_commits.length() > 0 ) {
-                out_commits += ",";
-            }
-            out_commits += commits;
-        }
-
-        outputMap.put( "out_commits", out_commits );
-        outputMap.put( "out_requires_review", getRequiresReviewCheckBox().getValue() );
-        getFormParamsEvent.fire( new GetFormParamsEvent( event.getAction(), outputMap ) );
+        initWidget(uiBinder.createAndBindUi(this));
 
     }
 
     @Override
-    public void init( SelectAssetsToPromotePresenter presenter ) {
+    public void init( final SelectAssetsToPromotePresenter presenter ) {
         this.presenter = presenter;
-
-        filesToPromoteList.addClickHandler( new ClickHandler() {
-            @Override
-            public void onClick( ClickEvent event ) {
-                if( filesToPromoteList.getSelectedIndex() != -1 ) {
-                    String value = filesToPromoteList.getSelectedValue();
-                    filesInTheBranchList.addItem( value );
-                    filesToPromoteList.removeItem( filesToPromoteList.getSelectedIndex() );
-                }
-            }
-        } );
-
     }
 
     @Override
-    public void displayNotification( String text ) {
-        notification.fire( new NotificationEvent( text ) );
+    public void clearFilesInBranch() {
+        filesInTheBranchList.clear();
     }
 
-    public TextBox getSourceBranchBox() {
-        return sourceBranchBox;
+    @Override
+    public void addFieldInBranch( String file ) {
+        filesInTheBranchList.addItem( file );
+    }
+
+    @Override
+    public void clearFilesToPromote() {
+        filesToPromoteList.clear();
+    }
+
+    @Override
+    public void addFileToPromote( String file ) {
+        filesToPromoteList.addItem(file);
+    }
+
+    @Override
+    public void setSourceBranch( String branch ) {
+        sourceBranchBox.setText( branch );
     }
 
     @UiHandler("promoteSelectedFilesButton")
     public void promoteSelectedFilesButton( ClickEvent e ) {
-        int selectedIndex = filesInTheBranchList.getSelectedIndex();
-        String value = filesInTheBranchList.getValue( selectedIndex );
-        filesToPromoteList.addItem( value );
-        filesInTheBranchList.removeItem( selectedIndex );
-
+        for (int i = 0; i < filesInTheBranchList.getItemCount(); i++) {
+            if (filesInTheBranchList.isItemSelected( i )) {
+                String file = filesInTheBranchList.getValue( i );
+                presenter.addFileToPromotedList( file );
+                filesInTheBranchList.removeItem( i );
+                i--;
+            }
+        }
     }
 
     @UiHandler("promoteAllFilesButton")
     public void promoteAllFilesButton( ClickEvent e ) {
-        int itemCount = filesInTheBranchList.getItemCount();
-        for ( int i = 0; i < itemCount; i++ ) {
-            String value = filesInTheBranchList.getValue( i );
-            filesToPromoteList.addItem( value );
+        for (int i = 0; i < filesInTheBranchList.getItemCount(); i++) {
+            String file = filesInTheBranchList.getValue( i );
+            presenter.addFileToPromotedList( file );
         }
         filesInTheBranchList.clear();
+    }
 
+    @UiHandler("filesToPromoteList")
+    public void removeFileFromPromotedList( ClickEvent e ) {
+        int selectedIndex = filesToPromoteList.getSelectedIndex();
+        String file = filesToPromoteList.getValue(selectedIndex);
+        filesToPromoteList.removeItem(selectedIndex);
+        presenter.removeFileFromPromotedList(file);
+    }
+
+    @UiHandler("requiresReviewCheckBox")
+    public void checkRequiredReviewCheckBox( ClickEvent e ) {
+        presenter.setRequiresReview( requiresReviewCheckBox.getValue() );
     }
 
     @Override
-    public ListBox getFilesInTheBranchList() {
-        return filesInTheBranchList;
-    }
-
-    @Override
-    public ListBox getFilesToPromoteList() {
-        return filesToPromoteList;
-    }
-
-    @Override
-    public CheckBox getRequiresReviewCheckBox() {
-        return requiresReviewCheckBox;
-    }
-
-    public void setInputMap( @Observes SetFormParamsEvent event ) {
-        Map<String, String> params = event.getParams();
-        String files = params.get( "in_list_of_files" );
-        commitsPerFile = new HashMap<String, String>();
-
-        String in_commits_per_file = params.get( "in_commits_per_file" );
-
-        JSONObject jsonCommitsPerFile = JSONParser.parseStrict( in_commits_per_file ).isObject();
-
-        if (jsonCommitsPerFile != null) {
-            for (String file : jsonCommitsPerFile.keySet()) {
-                StringBuffer fileCommits = new StringBuffer(  );
-                JSONArray commits = jsonCommitsPerFile.get( file ).isArray();
-                if (commits != null) {
-                    for (int i = 0; i < commits.size(); i++) {
-                        if (fileCommits.length() > 0) fileCommits.append( "," );
-                        fileCommits.append( commits.get( i ).isString().stringValue() );
-                    }
-                }
-                commitsPerFile.put( file, fileCommits.toString() );
-            }
-        }
-
-        String[] filesArray = files.split( "," );
-
-        getSourceBranchBox().setText( params.get( "in_source_branch_name" ) );
-
-        getFilesInTheBranchList().clear();
-        for ( String file : filesArray ) {
-            getFilesInTheBranchList().addItem( file );
-        }
-        setReadOnly( event.isReadOnly() );
-
-    }
-
-    private void setReadOnly( boolean readOnly ) {
+    public void setReadOnly( boolean readOnly ) {
         this.isReadOnly = readOnly;
-        if ( isReadOnly ) {
-            getFilesInTheBranchList().setEnabled( false );
-            getFilesToPromoteList().setEnabled( false );
-            promoteSelectedFilesButton.setEnabled( false );
-            promoteAllFilesButton.setEnabled( false );
-            requiresReviewCheckBox.setEnabled( false );
-            sourceBranchBox.setEnabled( false );
-        } else {
-            getFilesInTheBranchList().setEnabled( true );
-            getFilesToPromoteList().setEnabled( true );
-            promoteSelectedFilesButton.setEnabled( true );
-            promoteAllFilesButton.setEnabled( true );
-            requiresReviewCheckBox.setEnabled( true );
-            sourceBranchBox.setEnabled( true );
-        }
-
+        filesInTheBranchList.setEnabled( !readOnly );
+        filesToPromoteList.setEnabled( !readOnly );
+        promoteSelectedFilesButton.setEnabled( !readOnly );
+        promoteAllFilesButton.setEnabled( !readOnly );
+        requiresReviewCheckBox.setEnabled( !readOnly );
+        sourceBranchBox.setEnabled( !readOnly );
     }
-
-    public boolean isReadOnly() {
-        return this.isReadOnly;
-    }
-
 }
