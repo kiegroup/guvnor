e4baa16ca26d36ee68800907f39be2bc148cbd7f
Toni Rikkola <toni@rikkola.net>
2016-01-20 14:30:16 +0200

    GUVNOR-2345 : Remove test setup redundancy

--- guvnor-test-utils/src/main/java/org/guvnor/test/CDITestSetup.java
+++ guvnor-test-utils/src/main/java/org/guvnor/test/CDITestSetup.java
@@ -1,35 +1,36 @@
 /*
- * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
-package org.guvnor.common.services.project.backend.server;
+ */
+
+package org.guvnor.test;
 
 import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
 
 import org.jboss.weld.environment.se.StartMain;
-import org.junit.After;
 import org.uberfire.backend.server.util.Paths;
 import org.uberfire.java.nio.fs.file.SimpleFileSystemProvider;
 
-public class CDITestBase {
+public class CDITestSetup {
 
-    protected final SimpleFileSystemProvider fs = new SimpleFileSystemProvider();
-    protected BeanManager beanManager;
-    protected Paths paths;
+    public final SimpleFileSystemProvider fileSystemProvider = new SimpleFileSystemProvider();
+    public BeanManager beanManager;
+    public Paths       paths;
 
-    protected void setUp() throws Exception {
+    public void setUp() throws Exception {
         //Bootstrap WELD container
         StartMain startMain = new StartMain( new String[0] );
         beanManager = startMain.go().getBeanManager();
@@ -38,21 +39,15 @@ public class CDITestBase {
         paths = getReference( Paths.class );
 
         //Ensure URLs use the default:// scheme
-        fs.forceAsDefault();
+        fileSystemProvider.forceAsDefault();
 
     }
 
-    @After
-    public void tearDown() throws Exception {
-        TestAppSetup.reset();
-    }
-    
-    protected <T> T getReference( Class<T> clazz ) {
-        Bean bean = (Bean) beanManager.getBeans( clazz ).iterator().next();
+    public <T> T getReference( Class<T> clazz ) {
+        Bean bean = ( Bean ) beanManager.getBeans( clazz ).iterator().next();
         CreationalContext cc = beanManager.createCreationalContext( bean );
-        return (T) beanManager.getReference( bean,
-                                             clazz,
-                                             cc );
+        return ( T ) beanManager.getReference( bean,
+                                               clazz,
+                                               cc );
     }
-
 }
