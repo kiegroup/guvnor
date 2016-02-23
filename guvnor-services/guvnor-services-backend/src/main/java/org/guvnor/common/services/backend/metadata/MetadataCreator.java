5d48a98d7fd12b8344778801aaf81a6380d5d6fa
Michael Anstis <michael.anstis@gmail.com>
2016-02-23 15:36:09 +0000

    GUVNOR-2399: Errors retrieving LockInfo
     * closes #260

--- guvnor-services/guvnor-services-backend/src/main/java/org/guvnor/common/services/backend/metadata/MetadataCreator.java
+++ guvnor-services/guvnor-services-backend/src/main/java/org/guvnor/common/services/backend/metadata/MetadataCreator.java
@@ -16,8 +16,6 @@
 
 package org.guvnor.common.services.backend.metadata;
 
-import static org.uberfire.commons.validation.PortablePreconditions.checkNotNull;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -36,8 +34,11 @@ import org.uberfire.java.nio.base.version.VersionAttributeView;
 import org.uberfire.java.nio.base.version.VersionAttributes;
 import org.uberfire.java.nio.base.version.VersionHistory;
 import org.uberfire.java.nio.base.version.VersionRecord;
+import org.uberfire.java.nio.file.NoSuchFileException;
 import org.uberfire.java.nio.file.Path;
 import org.uberfire.rpc.SessionInfo;
+
+import static org.uberfire.commons.validation.PortablePreconditions.*;
 
 public class MetadataCreator {
 
@@ -49,47 +50,47 @@ public class MetadataCreator {
     private final IOService configIOService;
     private final SessionInfo sessionInfo;
 
-    public MetadataCreator(Path path,
-                           IOService configIOService,
-                           SessionInfo sessionInfo,
-                           DublinCoreView dublinCoreView,
-                           DiscussionView discussionView,
-                           OtherMetaView otherMetaView,
-                           VersionAttributeView versionAttributeView) {
-        this.path = checkNotNull("path", path);
-        this.configIOService = checkNotNull("configIOService", configIOService);
-        this.sessionInfo = checkNotNull("sessionInfo", sessionInfo);
-        this.dublinCoreView = checkNotNull("dublinCoreView", dublinCoreView);
-        this.discussView = checkNotNull("discussionView", discussionView);
-        this.otherMetaView = checkNotNull("otherMetaView", otherMetaView);
-        this.versionAttributeView = checkNotNull("versionAttributeView", versionAttributeView);
+    public MetadataCreator( Path path,
+                            IOService configIOService,
+                            SessionInfo sessionInfo,
+                            DublinCoreView dublinCoreView,
+                            DiscussionView discussionView,
+                            OtherMetaView otherMetaView,
+                            VersionAttributeView versionAttributeView ) {
+        this.path = checkNotNull( "path", path );
+        this.configIOService = checkNotNull( "configIOService", configIOService );
+        this.sessionInfo = checkNotNull( "sessionInfo", sessionInfo );
+        this.dublinCoreView = checkNotNull( "dublinCoreView", dublinCoreView );
+        this.discussView = checkNotNull( "discussionView", discussionView );
+        this.otherMetaView = checkNotNull( "otherMetaView", otherMetaView );
+        this.versionAttributeView = checkNotNull( "versionAttributeView", versionAttributeView );
     }
 
     public Metadata create() {
         return MetadataBuilder.newMetadata()
-                .withPath(Paths.convert(path))
-                .withRealPath(Paths.convert(path.toRealPath()))
-                .withCheckinComment(getCheckinComment())
-                .withLastContributor(getLastContributor())
-                .withCreator(getCreator())
-                .withLastModified(getLastModified())
-                .withDateCreated(getDateCreated())
-                .withSubject(getSubject())
-                .withType(getType())
-                .withExternalRelation(getExternalRelation())
-                .withExternalSource(getExternalSource())
-                .withDescription(getDescription())
+                .withPath( Paths.convert( path ) )
+                .withRealPath( Paths.convert( path.toRealPath() ) )
+                .withCheckinComment( getCheckinComment() )
+                .withLastContributor( getLastContributor() )
+                .withCreator( getCreator() )
+                .withLastModified( getLastModified() )
+                .withDateCreated( getDateCreated() )
+                .withSubject( getSubject() )
+                .withType( getType() )
+                .withExternalRelation( getExternalRelation() )
+                .withExternalSource( getExternalSource() )
+                .withDescription( getDescription() )
                 .withTags( getTags() )
-                .withDiscussion(getDiscussion())
-                .withLockInfo(retrieveLockInfo(Paths.convert(path)))
-                .withVersion(getVersion())
+                .withDiscussion( getDiscussion() )
+                .withLockInfo( retrieveLockInfo( Paths.convert( path ) ) )
+                .withVersion( getVersion() )
                 .build();
     }
 
     private ArrayList<VersionRecord> getVersion() {
-        return new ArrayList<VersionRecord>(versionAttributeView.readAttributes().history().records().size()) {{
-            for (final VersionRecord record : versionAttributeView.readAttributes().history().records()) {
-                add(new PortableVersionRecord(record.id(), record.author(), record.email(), record.comment(), record.date(), record.uri()));
+        return new ArrayList<VersionRecord>( versionAttributeView.readAttributes().history().records().size() ) {{
+            for ( final VersionRecord record : versionAttributeView.readAttributes().history().records() ) {
+                add( new PortableVersionRecord( record.id(), record.author(), record.email(), record.comment(), record.date(), record.uri() ) );
             }
         }};
     }
@@ -103,44 +104,44 @@ public class MetadataCreator {
     }
 
     private String getDescription() {
-        return dublinCoreView.readAttributes().descriptions().size() > 0 ? dublinCoreView.readAttributes().descriptions().get(0) : null;
+        return dublinCoreView.readAttributes().descriptions().size() > 0 ? dublinCoreView.readAttributes().descriptions().get( 0 ) : null;
     }
 
     private String getExternalSource() {
-        return dublinCoreView.readAttributes().sources().size() > 0 ? dublinCoreView.readAttributes().sources().get(0) : null;
+        return dublinCoreView.readAttributes().sources().size() > 0 ? dublinCoreView.readAttributes().sources().get( 0 ) : null;
     }
 
     private String getExternalRelation() {
-        return dublinCoreView.readAttributes().relations().size() > 0 ? dublinCoreView.readAttributes().relations().get(0) : null;
+        return dublinCoreView.readAttributes().relations().size() > 0 ? dublinCoreView.readAttributes().relations().get( 0 ) : null;
     }
 
     private String getType() {
-        return dublinCoreView.readAttributes().types().size() > 0 ? dublinCoreView.readAttributes().types().get(0) : null;
+        return dublinCoreView.readAttributes().types().size() > 0 ? dublinCoreView.readAttributes().types().get( 0 ) : null;
     }
 
     private String getSubject() {
-        return dublinCoreView.readAttributes().subjects().size() > 0 ? dublinCoreView.readAttributes().subjects().get(0) : null;
+        return dublinCoreView.readAttributes().subjects().size() > 0 ? dublinCoreView.readAttributes().subjects().get( 0 ) : null;
     }
 
     private Date getDateCreated() {
-        return new Date(versionAttributeView.readAttributes().creationTime().toMillis());
+        return new Date( versionAttributeView.readAttributes().creationTime().toMillis() );
     }
 
     private Date getLastModified() {
-        return new Date(versionAttributeView.readAttributes().lastModifiedTime().toMillis());
+        return new Date( versionAttributeView.readAttributes().lastModifiedTime().toMillis() );
     }
 
     private String getCreator() {
-        if (versionAttributeView.readAttributes().history().records().size() > 0) {
-            return versionAttributeView.readAttributes().history().records().get(0).author();
+        if ( versionAttributeView.readAttributes().history().records().size() > 0 ) {
+            return versionAttributeView.readAttributes().history().records().get( 0 ).author();
         } else {
             return null;
         }
     }
 
     private String getLastContributor() {
-        if (versionAttributeView.readAttributes().history().records().size() > 0) {
-            return versionAttributeView.readAttributes().history().records().get(versionAttributeView.readAttributes().history().records().size() - 1).author();
+        if ( versionAttributeView.readAttributes().history().records().size() > 0 ) {
+            return versionAttributeView.readAttributes().history().records().get( versionAttributeView.readAttributes().history().records().size() - 1 ).author();
         } else {
             return null;
         }
@@ -150,25 +151,27 @@ public class MetadataCreator {
         VersionAttributes versionAttributes = versionAttributeView.readAttributes();
         VersionHistory history = versionAttributes.history();
         List<VersionRecord> records = history.records();
-        if (records.size() > 0) {
-            return versionAttributeView.readAttributes().history().records().get(versionAttributeView.readAttributes().history().records().size() - 1).comment();
+        if ( records.size() > 0 ) {
+            return versionAttributeView.readAttributes().history().records().get( versionAttributeView.readAttributes().history().records().size() - 1 ).comment();
         } else {
             return null;
         }
     }
 
-    private LockInfo retrieveLockInfo(org.uberfire.backend.vfs.Path path) {
-        final org.uberfire.java.nio.file.Path lockPath = Paths.convert(PathFactory.newLock(path));
-        if (!configIOService.exists(lockPath)) {
-            return new LockInfo(false,
-                                "",
-                                path);
-        }
+    private LockInfo retrieveLockInfo( org.uberfire.backend.vfs.Path path ) {
+        final org.uberfire.java.nio.file.Path lockPath = Paths.convert( PathFactory.newLock( path ) );
+        try {
+            //See https://issues.jboss.org/browse/GUVNOR-2399. We simply try to read the lock file returning a default.
+            final String lockedBy = configIOService.readAllString( lockPath );
+            return new LockInfo( true,
+                                 lockedBy,
+                                 path );
 
-        final String lockedBy = configIOService.readAllString(lockPath);
-        return new LockInfo(true,
-                            lockedBy,
-                            path);
+        } catch ( NoSuchFileException nsfe ) {
+            return new LockInfo( false,
+                                 "",
+                                 path );
+        }
     }
 
 }
