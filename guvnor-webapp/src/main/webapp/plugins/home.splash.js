7e7544eef9df97cf4e3989b2c8d37218a5244228
Petr Široký <siroky.petr@gmail.com>
2016-05-10 18:33:55 +0200

    Remove unused guvnor-webapp (#313)

--- guvnor-webapp/src/main/webapp/plugins/home.splash.js
+++
@@ -1,9 +0,0 @@
-$registerSplashScreen({
-    id: 'home.splash',
-    templateUrl: "home.splash.html",
-    title: function () {
-        return 'Welcome';
-    },
-    display_next_time: true,
-    interception_points: ['org.guvnor.DefaultPerspective']
-});
