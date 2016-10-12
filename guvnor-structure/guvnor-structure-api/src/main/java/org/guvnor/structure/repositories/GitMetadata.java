a7525db5bf82c4cd4896abef9f8cbd96ef470612
Adriel Paredes <adriel.paredes@gmail.com>
2016-10-12 19:55:59 -0300

    [UF-408]: Pull Request Service added (#368)

--- guvnor-structure/guvnor-structure-api/src/main/java/org/guvnor/structure/repositories/GitMetadata.java
+++ guvnor-structure/guvnor-structure-api/src/main/java/org/guvnor/structure/repositories/GitMetadata.java
@@ -17,12 +17,59 @@
 package org.guvnor.structure.repositories;
 
 import java.util.List;
+import java.util.function.Predicate;
 
+/**
+ * Represents information about a repository. It contains the origin name
+ * the forks it has, the repository name,
+ * and the list of pull request that repository has.
+ */
 public interface GitMetadata {
 
+    /**
+     * Returns repository name
+     * @return the repository name
+     */
     String getName();
 
+    /**
+     * Returns the list of forks names it has
+     * @return the list of forks names
+     */
     List<String> getForks();
 
+    /**
+     * The name of the its origin repository
+     * @return
+     */
     String getOrigin();
+
+    /**
+     * The complete list of pull requests. It does not filter by status.
+     * @return the list of pull requests.
+     */
+    List<PullRequest> getPullRequests();
+
+    /**
+     * A filtered pull request list.
+     * @param filter the filter expression
+     * @return the filtered pull request list.
+     */
+    List<PullRequest> getPullRequests( Predicate<? super PullRequest> filter );
+
+    /**
+     * Return a single pull request by id
+     * @param id the pull request id
+     * @return the pull request found
+     */
+    PullRequest getPullRequest( long id );
+
+    /**
+     * Return if pull request already exists comparing
+     * branches name, repositories name and statuses
+     * but does not check pull request ID.
+     * @param pullRequest the pull request to check
+     * @return if pull request is present in metadata, returns true.
+     */
+    boolean exists( PullRequest pullRequest );
 }
